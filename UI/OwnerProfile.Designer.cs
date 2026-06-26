cc867e0e872202cc8fb1e79331158cbad05383f5
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:03:25 +0500

    Add files via upload

---
+++ UI/OwnerProfile.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace hostel_management_system_oop
+{
+    partial class OwnerProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl2f4 = new System.Windows.Forms.Label();
+            this.lbl3f4 = new System.Windows.Forms.Label();
+            this.lbl4f4 = new System.Windows.Forms.Label();
+            this.lbl5f4 = new System.Windows.Forms.Label();
+            this.lbl6f4 = new System.Windows.Forms.Label();
+            this.lbl7f4 = new System.Windows.Forms.Label();
+            this.lbl8f4 = new System.Windows.Forms.Label();
+            this.lbl9f4 = new System.Windows.Forms.Label();
+            this.lbl10f4 = new System.Windows.Forms.Label();
+            this.lbl11f4 = new System.Windows.Forms.Label();
+            this.lbl12f4 = new System.Windows.Forms.Label();
+            this.lbl13f4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl1f4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl2f4
+            // 
+            this.lbl2f4.AutoSize = true;
+            this.lbl2f4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl2f4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2f4.Location = new System.Drawing.Point(444, 212);
+            this.lbl2f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl2f4.Name = "lbl2f4";
+            this.lbl2f4.Size = new System.Drawing.Size(106, 25);
+            this.lbl2f4.TabIndex = 2;
+            this.lbl2f4.Text = "Full Name:";
+            // 
+            // lbl3f4
+            // 
+            this.lbl3f4.AutoSize = true;
+            this.lbl3f4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl3f4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3f4.Location = new System.Drawing.Point(446, 260);
+            this.lbl3f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl3f4.Name = "lbl3f4";
+            this.lbl3f4.Size = new System.Drawing.Size(106, 25);
+            this.lbl3f4.TabIndex = 3;
+            this.lbl3f4.Text = "Username:";
+            // 
+            // lbl4f4
+            // 
+            this.lbl4f4.AutoSize = true;
+            this.lbl4f4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl4f4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4f4.Location = new System.Drawing.Point(451, 311);
+            this.lbl4f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl4f4.Name = "lbl4f4";
+            this.lbl4f4.Size = new System.Drawing.Size(102, 25);
+            this.lbl4f4.TabIndex = 4;
+            this.lbl4f4.Text = "Password:";
+            // 
+            // lbl5f4
+            // 
+            this.lbl5f4.AutoSize = true;
+            this.lbl5f4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl5f4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5f4.Location = new System.Drawing.Point(486, 358);
+            this.lbl5f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl5f4.Name = "lbl5f4";
+            this.lbl5f4.Size = new System.Drawing.Size(72, 25);
+            this.lbl5f4.TabIndex = 5;
+            this.lbl5f4.Text = "E-mail:";
+            // 
+            // lbl6f4
+            // 
+            this.lbl6f4.AutoSize = true;
+            this.lbl6f4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl6f4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl6f4.Location = new System.Drawing.Point(404, 410);
+            this.lbl6f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl6f4.Name = "lbl6f4";
+            this.lbl6f4.Size = new System.Drawing.Size(154, 25);
+            this.lbl6f4.TabIndex = 6;
+            this.lbl6f4.Text = "Phone Number:";
+            // 
+            // lbl7f4
+            // 
+            this.lbl7f4.AutoSize = true;
+            this.lbl7f4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl7f4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl7f4.Location = new System.Drawing.Point(446, 166);
+            this.lbl7f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl7f4.Name = "lbl7f4";
+            this.lbl7f4.Size = new System.Drawing.Size(101, 25);
+            this.lbl7f4.TabIndex = 7;
+            this.lbl7f4.Text = "Owner ID:";
+            // 
+            // lbl8f4
+            // 
+            this.lbl8f4.AutoSize = true;
+            this.lbl8f4.BackColor = System.Drawing.Color.White;
+            this.lbl8f4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl8f4.Location = new System.Drawing.Point(597, 166);
+            this.lbl8f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl8f4.Name = "lbl8f4";
+            this.lbl8f4.Size = new System.Drawing.Size(0, 25);
+            this.lbl8f4.TabIndex = 8;
+            // 
+            // lbl9f4
+            // 
+            this.lbl9f4.AutoSize = true;
+            this.lbl9f4.BackColor = System.Drawing.Color.White;
+            this.lbl9f4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl9f4.Location = new System.Drawing.Point(597, 212);
+            this.lbl9f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl9f4.Name = "lbl9f4";
+            this.lbl9f4.Size = new System.Drawing.Size(0, 25);
+            this.lbl9f4.TabIndex = 9;
+            // 
+            // lbl10f4
+            // 
+            this.lbl10f4.AutoSize = true;
+            this.lbl10f4.BackColor = System.Drawing.Color.White;
+            this.lbl10f4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl10f4.Location = new System.Drawing.Point(597, 260);
+            this.lbl10f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl10f4.Name = "lbl10f4";
+            this.lbl10f4.Size = new System.Drawing.Size(0, 25);
+            this.lbl10f4.TabIndex = 10;
+            // 
+            // lbl11f4
+            // 
+            this.lbl11f4.AutoSize = true;
+            this.lbl11f4.BackColor = System.Drawing.Color.White;
+            this.lbl11f4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl11f4.Location = new System.Drawing.Point(597, 311);
+            this.lbl11f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl11f4.Name = "lbl11f4";
+            this.lbl11f4.Size = new System.Drawing.Size(0, 25);
+            this.lbl11f4.TabIndex = 11;
+            // 
+            // lbl12f4
+            // 
+            this.lbl12f4.AutoSize = true;
+            this.lbl12f4.BackColor = System.Drawing.Color.White;
+            this.lbl12f4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl12f4.Location = new System.Drawing.Point(597, 358);
+            this.lbl12f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl12f4.Name = "lbl12f4";
+            this.lbl12f4.Size = new System.Drawing.Size(0, 25);
+            this.lbl12f4.TabIndex = 12;
+            // 
+            // lbl13f4
+            // 
+            this.lbl13f4.AutoSize = true;
+            this.lbl13f4.BackColor = System.Drawing.Color.White;
+            this.lbl13f4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl13f4.Location = new System.Drawing.Point(597, 410);
+            this.lbl13f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl13f4.Name = "lbl13f4";
+            this.lbl13f4.Size = new System.Drawing.Size(0, 25);
+            this.lbl13f4.TabIndex = 13;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.lbl1f4);
+            this.panel1.Location = new System.Drawing.Point(-2, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1630, 102);
+            this.panel1.TabIndex = 14;
+            // 
+            // lbl1f4
+            // 
+            this.lbl1f4.AutoSize = true;
+            this.lbl1f4.Font = new System.Drawing.Font("Segoe UI", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1f4.Location = new System.Drawing.Point(473, 29);
+            this.lbl1f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl1f4.Name = "lbl1f4";
+            this.lbl1f4.Size = new System.Drawing.Size(247, 47);
+            this.lbl1f4.TabIndex = 2;
+            this.lbl1f4.Text = "Owner Profile";
+            // 
+            // OwnerProfile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(1155, 687);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbl13f4);
+            this.Controls.Add(this.lbl12f4);
+            this.Controls.Add(this.lbl11f4);
+            this.Controls.Add(this.lbl10f4);
+            this.Controls.Add(this.lbl9f4);
+            this.Controls.Add(this.lbl8f4);
+            this.Controls.Add(this.lbl7f4);
+            this.Controls.Add(this.lbl6f4);
+            this.Controls.Add(this.lbl5f4);
+            this.Controls.Add(this.lbl4f4);
+            this.Controls.Add(this.lbl3f4);
+            this.Controls.Add(this.lbl2f4);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "OwnerProfile";
+            this.Text = "Form4";
+           
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl2f4;
+        private System.Windows.Forms.Label lbl3f4;
+        private System.Windows.Forms.Label lbl4f4;
+        private System.Windows.Forms.Label lbl5f4;
+        private System.Windows.Forms.Label lbl6f4;
+        private System.Windows.Forms.Label lbl7f4;
+        private System.Windows.Forms.Label lbl8f4;
+        private System.Windows.Forms.Label lbl9f4;
+        private System.Windows.Forms.Label lbl10f4;
+        private System.Windows.Forms.Label lbl11f4;
+        private System.Windows.Forms.Label lbl12f4;
+        private System.Windows.Forms.Label lbl13f4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl1f4;
+    }
+}
---
+++ UI/OwnerProfile.cs
@@ -0,0 +1,74 @@
+﻿using hostel_management_system_oop.BL;
+using hostel_management_system_oop.Model;
+using hostel_management_system_oop.UI;  // ← ADD THIS LINE
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace hostel_management_system_oop
+{
+    public partial class OwnerProfile : BaseForm  // ← CHANGE Form TO BaseForm
+    {
+        private OwnerProfileBL ownerBL = new OwnerProfileBL();
+        private string currentUsername;
+
+        public OwnerProfile()
+        {
+            InitializeComponent();
+            currentUsername = AdminLogin.LoggedInUsername;
+            LoadData();  
+        }
+
+       
+        public override void LoadData()
+        {
+            try
+            {
+                DataTable dt = ownerBL.GetOwnerByUsername(currentUsername);
+
+                if (dt.Rows.Count > 0)
+                {
+                    
+                    lbl8f4.Text = dt.Rows[0]["OwnerID"].ToString();
+                    lbl9f4.Text = dt.Rows[0]["FullName"].ToString();
+                    lbl10f4.Text = dt.Rows[0]["Username"].ToString();
+                    lbl11f4.Text = dt.Rows[0]["Password"].ToString();
+                    lbl12f4.Text = dt.Rows[0]["Email"].ToString();
+                    lbl13f4.Text = dt.Rows[0]["PhoneNumber"].ToString();
+                }
+                else
+                {
+                    SetDefaultProfile();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading profile: " + ex.Message);
+                SetDefaultProfile();
+            }
+        }
+
+        
+        public override void ClearForm()
+        {
+            
+        }
+
+      
+        public override bool ValidateForm()
+        {
+            
+            return true;
+        }
+
+        private void SetDefaultProfile()
+        {
+            lbl8f4.Text = "Auto";
+            lbl9f4.Text = currentUsername;
+            lbl10f4.Text = currentUsername;
+            lbl11f4.Text = "****";
+            lbl12f4.Text = "Not provided";
+            lbl13f4.Text = "Not provided";
+        }
+    }
+}
