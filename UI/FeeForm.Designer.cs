43134492ccc59ac4563440d2c01e6238e551ab59
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:00:56 +0500

    Add files via upload

---
+++ UI/FeeForm.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace hostel_management_system_oop
+{
+    partial class FeeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbx3f = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn1f = new System.Windows.Forms.Button();
+            this.tbx2f = new System.Windows.Forms.TextBox();
+            this.tbx1f = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl1f4 = new System.Windows.Forms.Label();
+            this.dtpf = new System.Windows.Forms.DateTimePicker();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.btn2f = new System.Windows.Forms.Button();
+            this.btn3f = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbx3f
+            // 
+            this.tbx3f.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx3f.Location = new System.Drawing.Point(621, 232);
+            this.tbx3f.Name = "tbx3f";
+            this.tbx3f.Size = new System.Drawing.Size(257, 29);
+            this.tbx3f.TabIndex = 25;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(493, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 25);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Due Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(464, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 25);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Amount Paid";
+            // 
+            // btn1f
+            // 
+            this.btn1f.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1f.Location = new System.Drawing.Point(621, 388);
+            this.btn1f.Name = "btn1f";
+            this.btn1f.Size = new System.Drawing.Size(75, 35);
+            this.btn1f.TabIndex = 21;
+            this.btn1f.Text = "Save";
+            this.btn1f.UseVisualStyleBackColor = true;
+            this.btn1f.Click += new System.EventHandler(this.btn1f_Click);
+            // 
+            // tbx2f
+            // 
+            this.tbx2f.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx2f.Location = new System.Drawing.Point(621, 187);
+            this.tbx2f.Name = "tbx2f";
+            this.tbx2f.Size = new System.Drawing.Size(257, 29);
+            this.tbx2f.TabIndex = 20;
+            // 
+            // tbx1f
+            // 
+            this.tbx1f.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx1f.Location = new System.Drawing.Point(621, 136);
+            this.tbx1f.Name = "tbx1f";
+            this.tbx1f.Size = new System.Drawing.Size(257, 29);
+            this.tbx1f.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(464, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 25);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Amount Due";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(507, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 25);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Roll No";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.lbl1f4);
+            this.panel1.Location = new System.Drawing.Point(-59, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1600, 89);
+            this.panel1.TabIndex = 16;
+            // 
+            // lbl1f4
+            // 
+            this.lbl1f4.AutoSize = true;
+            this.lbl1f4.Font = new System.Drawing.Font("Segoe UI", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1f4.Location = new System.Drawing.Point(610, 28);
+            this.lbl1f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl1f4.Name = "lbl1f4";
+            this.lbl1f4.Size = new System.Drawing.Size(300, 47);
+            this.lbl1f4.TabIndex = 4;
+            this.lbl1f4.Text = "Fee Management";
+            // 
+            // dtpf
+            // 
+            this.dtpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpf.Location = new System.Drawing.Point(621, 281);
+            this.dtpf.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpf.Name = "dtpf";
+            this.dtpf.Size = new System.Drawing.Size(257, 26);
+            this.dtpf.TabIndex = 28;
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(451, 458);
+            this.dgv1.Margin = new System.Windows.Forms.Padding(2);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowHeadersWidth = 51;
+            this.dgv1.RowTemplate.Height = 24;
+            this.dgv1.Size = new System.Drawing.Size(566, 162);
+            this.dgv1.TabIndex = 29;
+            // 
+            // btn2f
+            // 
+            this.btn2f.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2f.Location = new System.Drawing.Point(717, 388);
+            this.btn2f.Name = "btn2f";
+            this.btn2f.Size = new System.Drawing.Size(75, 35);
+            this.btn2f.TabIndex = 30;
+            this.btn2f.Text = "Update";
+            this.btn2f.UseVisualStyleBackColor = true;
+            this.btn2f.Click += new System.EventHandler(this.btn2f_Click_1);
+            // 
+            // btn3f
+            // 
+            this.btn3f.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3f.Location = new System.Drawing.Point(803, 388);
+            this.btn3f.Name = "btn3f";
+            this.btn3f.Size = new System.Drawing.Size(75, 35);
+            this.btn3f.TabIndex = 31;
+            this.btn3f.Text = "Delete";
+            this.btn3f.UseVisualStyleBackColor = true;
+            this.btn3f.Click += new System.EventHandler(this.btn3f_Click_1);
+            // 
+            // FeeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(1155, 687);
+            this.Controls.Add(this.btn3f);
+            this.Controls.Add(this.btn2f);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.dtpf);
+            this.Controls.Add(this.tbx3f);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn1f);
+            this.Controls.Add(this.tbx2f);
+            this.Controls.Add(this.tbx1f);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FeeForm";
+            this.Text = "FeeForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbx3f;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn1f;
+        private System.Windows.Forms.TextBox tbx2f;
+        private System.Windows.Forms.TextBox tbx1f;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl1f4;
+        private System.Windows.Forms.DateTimePicker dtpf;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Button btn2f;
+        private System.Windows.Forms.Button btn3f;
+    }
+}
---
+++ UI/FeeForm.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using hostel_management_system_oop.BL;
+using hostel_management_system_oop.Model;
+using hostel_management_system_oop.UI;  
+namespace hostel_management_system_oop
+{
+    public partial class FeeForm : BaseForm  
+    {
+        private FeeBL feeBL = new FeeBL();
+        private int selectedFeeID = -1;
+
+        public FeeForm()
+        {
+            InitializeComponent();
+            SetupDataGridView();
+            dtpf.Value = DateTime.Now.AddMonths(1);
+            LoadData(); 
+        }
+
+       
+        public override void LoadData()
+        {
+            try
+            {
+                DataTable dt = feeBL.GetAllFees();
+                dgv1.Rows.Clear();
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    dgv1.Rows.Add(
+                        row["FeeID"].ToString(),
+                        row["RollNumber"].ToString(),
+                        row["AmountDue"].ToString(),
+                        row["AmountPaid"].ToString(),
+                        Convert.ToDateTime(row["DueDate"]).ToString("dd/MM/yyyy"),
+                        row["PaymentStatus"].ToString()
+                    );
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading fees: " + ex.Message);
+            }
+        }
+
+       
+        public override void ClearForm()
+        {
+            tbx1f.Clear();
+            tbx2f.Clear();
+            tbx3f.Clear();
+            dtpf.Value = DateTime.Now.AddMonths(1);
+            tbx1f.Focus();
+            selectedFeeID = -1;
+        }
+
+       
+        public override bool ValidateForm()
+        {
+            if (string.IsNullOrEmpty(tbx1f.Text))
+            {
+                MessageBox.Show("Please enter Roll Number!");
+                tbx1f.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(tbx2f.Text))
+            {
+                MessageBox.Show("Please enter Amount Due!");
+                tbx2f.Focus();
+                return false;
+            }
+
+            int amountDue;
+            if (!int.TryParse(tbx2f.Text, out amountDue) || amountDue <= 0)
+            {
+                MessageBox.Show("Amount Due must be greater than zero!");
+                tbx2f.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void SetupDataGridView()
+        {
+            dgv1.AutoGenerateColumns = false;
+            dgv1.Columns.Clear();
+
+            dgv1.Columns.Add("FeeID", "ID");
+            dgv1.Columns.Add("RollNumber", "Roll No");
+            dgv1.Columns.Add("AmountDue", "Amount Due");
+            dgv1.Columns.Add("AmountPaid", "Amount Paid");
+            dgv1.Columns.Add("DueDate", "Due Date");
+            dgv1.Columns.Add("PaymentStatus", "Status");
+
+            dgv1.Columns["FeeID"].Width = 50;
+            dgv1.Columns["RollNumber"].Width = 100;
+            dgv1.Columns["AmountDue"].Width = 100;
+            dgv1.Columns["AmountPaid"].Width = 100;
+            dgv1.Columns["DueDate"].Width = 120;
+            dgv1.Columns["PaymentStatus"].Width = 80;
+
+            dgv1.AllowUserToAddRows = false;
+            dgv1.ReadOnly = true;
+            dgv1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv1.RowHeadersVisible = false;
+
+            dgv1.CellClick += dgv1_CellClick;
+        }
+
+        private void dgv1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgv1.Rows[e.RowIndex];
+
+                selectedFeeID = Convert.ToInt32(row.Cells["FeeID"].Value);
+
+                tbx1f.Text = row.Cells["RollNumber"].Value.ToString();
+                tbx2f.Text = row.Cells["AmountDue"].Value.ToString();
+                tbx3f.Text = row.Cells["AmountPaid"].Value.ToString();
+                dtpf.Value = Convert.ToDateTime(row.Cells["DueDate"].Value);
+            }
+        }
+
+       
+        private string CalculateStatus(int amountDue, int amountPaid)
+        {
+           
+            if (amountDue <= 0)
+                return "Invalid (Due > 0)";
+
+            
+            if (amountPaid > amountDue)
+                return "Invalid (Paid > Due)";
+
+            // Logic
+            if (amountPaid == 0)
+                return "Pending";
+            else if (amountPaid < amountDue)
+                return "Partial";
+            else
+                return "Paid";
+        }
+
+       
+        private bool ValidateAmounts(int amountDue, int amountPaid)
+        {
+            if (amountDue <= 0)
+            {
+                MessageBox.Show("Amount Due must be greater than zero!");
+                return false;
+            }
+
+            if (amountPaid > amountDue)
+            {
+                MessageBox.Show("Amount Paid cannot be greater than Amount Due!");
+                return false;
+            }
+
+            return true;
+        }
+
+        // SAVE Button
+        private void btn1f_Click(object sender, EventArgs e)
+        {
+           
+            if (!ValidateForm())
+                return;
+
+            int amountDue, amountPaid = 0;
+            if (!int.TryParse(tbx2f.Text, out amountDue) || amountDue <= 0)
+            {
+                MessageBox.Show("Amount Due must be greater than zero!");
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(tbx3f.Text))
+            {
+                int.TryParse(tbx3f.Text, out amountPaid);
+            }
+
+           
+            if (!ValidateAmounts(amountDue, amountPaid))
+                return;
+
+            
+            string status = CalculateStatus(amountDue, amountPaid);
+
+            FeeModel fee = new FeeModel();
+            fee.RollNumber = tbx1f.Text;
+            fee.AmountDue = amountDue;
+            fee.AmountPaid = amountPaid;
+            fee.DueDate = dtpf.Value.Date;
+            fee.PaymentStatus = status;
+
+            string result = feeBL.SaveFee(fee);
+
+            if (result == "SUCCESS")
+            {
+                MessageBox.Show($"Fee Record Saved Successfully! Status: {status}");
+                ClearForm(); 
+                LoadData();   
+            }
+            else
+            {
+                MessageBox.Show("Error: " + result);
+            }
+        }
+
+       
+        private void btn2f_Click_1(object sender, EventArgs e)
+        {
+            if (selectedFeeID == -1)
+            {
+                MessageBox.Show("Please click on a row to select a fee record first!");
+                return;
+            }
+
+            
+            if (!ValidateForm())
+                return;
+
+            int amountDue, amountPaid = 0;
+            if (!int.TryParse(tbx2f.Text, out amountDue) || amountDue <= 0)
+            {
+                MessageBox.Show("Amount Due must be greater than zero!");
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(tbx3f.Text))
+            {
+                int.TryParse(tbx3f.Text, out amountPaid);
+            }
+
+           
+            if (!ValidateAmounts(amountDue, amountPaid))
+                return;
+
+            
+            string status = CalculateStatus(amountDue, amountPaid);
+
+            FeeModel fee = new FeeModel();
+            fee.FeeID = selectedFeeID;
+            fee.RollNumber = tbx1f.Text;
+            fee.AmountDue = amountDue;
+            fee.AmountPaid = amountPaid;
+            fee.DueDate = dtpf.Value.Date;
+            fee.PaymentStatus = status;
+
+            string result = feeBL.UpdateFee(fee);
+
+            if (result == "SUCCESS")
+            {
+                MessageBox.Show($"Fee Record Updated Successfully! Status: {status}");
+                ClearForm();
+                LoadData();
+                selectedFeeID = -1;
+            }
+            else
+            {
+                MessageBox.Show("Error: " + result);
+            }
+        }
+
+       
+        private void btn3f_Click_1(object sender, EventArgs e)
+        {
+            if (selectedFeeID == -1)
+            {
+                MessageBox.Show("Please click on a row to select a fee record first!");
+                return;
+            }
+
+            DialogResult confirm = MessageBox.Show("Are you sure you want to delete this fee record?", "Confirm Delete",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (confirm == DialogResult.Yes)
+            {
+                string result = feeBL.DeleteFee(selectedFeeID);
+
+                if (result == "SUCCESS")
+                {
+                    MessageBox.Show("Fee Record Deleted Successfully!");
+                    ClearForm();
+                    LoadData();
+                }
+                else
+                {
+                    MessageBox.Show("Error: " + result);
+                }
+            }
+        }
+    }
+}
