00f2e8c147a461d7ec0377d9068e10721fac9899
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:25:08 +0500

    Add files via upload

---
+++ Model/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hostel_management_system_oop.Model
+{
+    public class Student 
+    {
+      
+        public int StudentID { get; set; }
+        public string RollNumber { get; set; }
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string CNIC { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+       
+        public Student()
+        {
+            Gender = "Boy";
+        }
+    }
+}
