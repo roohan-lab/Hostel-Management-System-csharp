70e9df654e1bd865204a4164ebc83268ad235ce0
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:03:52 +0500

    Add files via upload

---
+++ UI/Room.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace hostel_management_system_oop
+{
+    partial class Room
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl1f4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbx1f10 = new System.Windows.Forms.ComboBox();
+            this.tbx1f10 = new System.Windows.Forms.TextBox();
+            this.tbx2f10 = new System.Windows.Forms.TextBox();
+            this.rb1f10 = new System.Windows.Forms.RadioButton();
+            this.tbx3f10 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btns = new System.Windows.Forms.Button();
+            this.rb2f10 = new System.Windows.Forms.RadioButton();
+            this.tbx4f10 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgvR = new System.Windows.Forms.DataGridView();
+            this.btnup = new System.Windows.Forms.Button();
+            this.btndel = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.lbl1f4);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(2104, 110);
+            this.panel1.TabIndex = 2;
+            // 
+            // lbl1f4
+            // 
+            this.lbl1f4.AutoSize = true;
+            this.lbl1f4.Font = new System.Drawing.Font("Segoe UI", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1f4.Location = new System.Drawing.Point(755, 30);
+            this.lbl1f4.Name = "lbl1f4";
+            this.lbl1f4.Size = new System.Drawing.Size(356, 59);
+            this.lbl1f4.TabIndex = 4;
+            this.lbl1f4.Text = "Room Allotment";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(765, 208);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Room No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(743, 448);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hostel Type:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(719, 326);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Total Capacity:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(659, 386);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(230, 31);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Current Occupation:";
+            // 
+            // cbx1f10
+            // 
+            this.cbx1f10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx1f10.FormattingEnabled = true;
+            this.cbx1f10.Location = new System.Drawing.Point(935, 263);
+            this.cbx1f10.Margin = new System.Windows.Forms.Padding(4);
+            this.cbx1f10.Name = "cbx1f10";
+            this.cbx1f10.Size = new System.Drawing.Size(269, 36);
+            this.cbx1f10.TabIndex = 7;
+            // 
+            // tbx1f10
+            // 
+            this.tbx1f10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx1f10.Location = new System.Drawing.Point(935, 153);
+            this.tbx1f10.Margin = new System.Windows.Forms.Padding(4);
+            this.tbx1f10.Name = "tbx1f10";
+            this.tbx1f10.Size = new System.Drawing.Size(269, 34);
+            this.tbx1f10.TabIndex = 8;
+            // 
+            // tbx2f10
+            // 
+            this.tbx2f10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx2f10.Location = new System.Drawing.Point(935, 203);
+            this.tbx2f10.Margin = new System.Windows.Forms.Padding(4);
+            this.tbx2f10.Name = "tbx2f10";
+            this.tbx2f10.Size = new System.Drawing.Size(269, 34);
+            this.tbx2f10.TabIndex = 9;
+            // 
+            // rb1f10
+            // 
+            this.rb1f10.AutoSize = true;
+            this.rb1f10.BackColor = System.Drawing.Color.White;
+            this.rb1f10.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb1f10.Location = new System.Drawing.Point(997, 453);
+            this.rb1f10.Margin = new System.Windows.Forms.Padding(4);
+            this.rb1f10.Name = "rb1f10";
+            this.rb1f10.Size = new System.Drawing.Size(60, 27);
+            this.rb1f10.TabIndex = 10;
+            this.rb1f10.TabStop = true;
+            this.rb1f10.Text = "Boy";
+            this.rb1f10.UseVisualStyleBackColor = false;
+            // 
+            // tbx3f10
+            // 
+            this.tbx3f10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx3f10.Location = new System.Drawing.Point(935, 326);
+            this.tbx3f10.Margin = new System.Windows.Forms.Padding(4);
+            this.tbx3f10.Name = "tbx3f10";
+            this.tbx3f10.Size = new System.Drawing.Size(269, 34);
+            this.tbx3f10.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(731, 263);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 31);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Room Status:";
+            // 
+            // btns
+            // 
+            this.btns.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btns.Location = new System.Drawing.Point(1027, 520);
+            this.btns.Margin = new System.Windows.Forms.Padding(4);
+            this.btns.Name = "btns";
+            this.btns.Size = new System.Drawing.Size(75, 46);
+            this.btns.TabIndex = 13;
+            this.btns.Text = "Save";
+            this.btns.UseVisualStyleBackColor = true;
+            this.btns.Click += new System.EventHandler(this.btns_Click_1);
+            // 
+            // rb2f10
+            // 
+            this.rb2f10.AutoSize = true;
+            this.rb2f10.BackColor = System.Drawing.Color.White;
+            this.rb2f10.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb2f10.Location = new System.Drawing.Point(1095, 453);
+            this.rb2f10.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rb2f10.Name = "rb2f10";
+            this.rb2f10.Size = new System.Drawing.Size(57, 27);
+            this.rb2f10.TabIndex = 14;
+            this.rb2f10.TabStop = true;
+            this.rb2f10.Text = "Girl";
+            this.rb2f10.UseVisualStyleBackColor = false;
+            // 
+            // tbx4f10
+            // 
+            this.tbx4f10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx4f10.Location = new System.Drawing.Point(935, 386);
+            this.tbx4f10.Margin = new System.Windows.Forms.Padding(4);
+            this.tbx4f10.Name = "tbx4f10";
+            this.tbx4f10.Size = new System.Drawing.Size(269, 34);
+            this.tbx4f10.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(715, 154);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(173, 31);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Student Name:";
+            // 
+            // dgvR
+            // 
+            this.dgvR.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvR.Location = new System.Drawing.Point(630, 667);
+            this.dgvR.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvR.Name = "dgvR";
+            this.dgvR.RowHeadersWidth = 51;
+            this.dgvR.Size = new System.Drawing.Size(798, 217);
+            this.dgvR.TabIndex = 17;
+            // 
+            // btnup
+            // 
+            this.btnup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnup.Location = new System.Drawing.Point(916, 520);
+            this.btnup.Name = "btnup";
+            this.btnup.Size = new System.Drawing.Size(94, 46);
+            this.btnup.TabIndex = 18;
+            this.btnup.Text = "Update";
+            this.btnup.UseVisualStyleBackColor = true;
+            this.btnup.Click += new System.EventHandler(this.btnup_Click_1);
+            // 
+            // btndel
+            // 
+            this.btndel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndel.Location = new System.Drawing.Point(1127, 520);
+            this.btndel.Name = "btndel";
+            this.btndel.Size = new System.Drawing.Size(85, 46);
+            this.btndel.TabIndex = 19;
+            this.btndel.Text = "Delete";
+            this.btndel.UseVisualStyleBackColor = true;
+            this.btndel.Click += new System.EventHandler(this.btndel_Click_1);
+            // 
+            // Room
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(1924, 1040);
+            this.Controls.Add(this.btndel);
+            this.Controls.Add(this.btnup);
+            this.Controls.Add(this.dgvR);
+            this.Controls.Add(this.tbx4f10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.rb2f10);
+            this.Controls.Add(this.btns);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbx3f10);
+            this.Controls.Add(this.rb1f10);
+            this.Controls.Add(this.tbx2f10);
+            this.Controls.Add(this.tbx1f10);
+            this.Controls.Add(this.cbx1f10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Room";
+            this.Text = "Form10";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl1f4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbx1f10;
+        private System.Windows.Forms.TextBox tbx1f10;
+        private System.Windows.Forms.TextBox tbx2f10;
+        private System.Windows.Forms.RadioButton rb1f10;
+        private System.Windows.Forms.TextBox tbx3f10;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btns;
+        private System.Windows.Forms.RadioButton rb2f10;
+        private System.Windows.Forms.TextBox tbx4f10;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgvR;
+        private System.Windows.Forms.Button btnup;
+        private System.Windows.Forms.Button btndel;
+    }
+}
---
+++ UI/Room.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using hostel_management_system_oop.BL;
+using hostel_management_system_oop.Model;
+using hostel_management_system_oop.UI;
+
+namespace hostel_management_system_oop
+{
+    public partial class Room : AbstractBaseForm
+    {
+        private RoomBL roomBL = new RoomBL();
+
+        public Room()
+        {
+            // ✅ FIX: Check if NOT in design mode
+            if (!DesignMode)
+            {
+                InitializeComponent();
+                this.Text = FormTitle;
+                SetupDataGridView();
+                LoadData();
+                LoadRoomStatus();
+            }
+            else
+            {
+                InitializeComponent();
+            }
+        }
+
+        public override string FormTitle
+        {
+            get { return "Room Management"; }
+        }
+
+        public override void LoadData()
+        {
+            try
+            {
+                DataTable dt = roomBL.GetAllRooms();
+                dgvR.Rows.Clear();
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    dgvR.Rows.Add(
+                        row["StudentName"].ToString(),
+                        row["RoomID"].ToString(),
+                        row["RoomNumber"].ToString(),
+                        row["HostelType"].ToString(),
+                        row["TotalCapacity"].ToString(),
+                        row["CurrentOccupancy"].ToString(),
+                        row["RoomStatus"].ToString()
+                    );
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading rooms: " + ex.Message);
+            }
+        }
+
+        public override void ClearForm()
+        {
+            tbx1f10.Clear();
+            tbx2f10.Clear();
+            rb1f10.Checked = false;
+            rb2f10.Checked = false;
+            tbx3f10.Clear();
+            tbx4f10.Clear();
+            cbx1f10.SelectedIndex = 0;
+            tbx2f10.Focus();
+        }
+
+        public override bool ValidateForm()
+        {
+            if (string.IsNullOrEmpty(tbx2f10.Text))
+            {
+                MessageBox.Show("Please enter Room Number!");
+                tbx2f10.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(tbx3f10.Text))
+            {
+                MessageBox.Show("Please enter Total Capacity!");
+                tbx3f10.Focus();
+                return false;
+            }
+
+            int capacity;
+            if (!int.TryParse(tbx3f10.Text, out capacity) || capacity <= 0)
+            {
+                MessageBox.Show("Please enter valid Total Capacity!");
+                tbx3f10.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void SetupDataGridView()
+        {
+            dgvR.AutoGenerateColumns = false;
+            dgvR.Columns.Clear();
+
+            // Add columns
+            dgvR.Columns.Add("StudentName", "Student Name");
+            dgvR.Columns.Add("RoomID", "ID");
+            dgvR.Columns.Add("RoomNumber", "Room No");
+            dgvR.Columns.Add("HostelType", "Hostel Type");
+            dgvR.Columns.Add("TotalCapacity", "Total Capacity");
+            dgvR.Columns.Add("CurrentOccupancy", "Current Occupancy");
+            dgvR.Columns.Add("RoomStatus", "Room Status");
+
+            // Set widths
+            dgvR.Columns["StudentName"].Width = 120;
+            dgvR.Columns["RoomID"].Width = 50;
+            dgvR.Columns["RoomNumber"].Width = 80;
+            dgvR.Columns["HostelType"].Width = 90;
+            dgvR.Columns["TotalCapacity"].Width = 100;
+            dgvR.Columns["CurrentOccupancy"].Width = 110;
+            dgvR.Columns["RoomStatus"].Width = 90;
+
+            dgvR.CellClick += dgvR_CellClick;
+        }
+
+        private void LoadRoomStatus()
+        {
+            cbx1f10.Items.Clear();
+            cbx1f10.Items.Add("Available");
+            cbx1f10.Items.Add("Full");
+            cbx1f10.Items.Add("Maintenance");
+            cbx1f10.SelectedIndex = 0;
+        }
+
+        private void dgvR_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvR.Rows[e.RowIndex];
+
+                tbx1f10.Text = row.Cells["StudentName"].Value.ToString();
+                tbx2f10.Text = row.Cells["RoomNumber"].Value.ToString();
+
+                string hostelType = row.Cells["HostelType"].Value.ToString();
+                rb1f10.Checked = (hostelType == "Boy");
+                rb2f10.Checked = (hostelType == "Girl");
+
+                tbx3f10.Text = row.Cells["TotalCapacity"].Value.ToString();
+                tbx4f10.Text = row.Cells["CurrentOccupancy"].Value.ToString();
+                cbx1f10.Text = row.Cells["RoomStatus"].Value.ToString();
+            }
+        }
+
+        // SAVE Button
+        private void btns_Click_1(object sender, EventArgs e)
+        {
+            if (!ValidateForm())
+                return;
+
+            RoomModel room = new RoomModel();
+            room.StudentName = tbx1f10.Text;
+            room.RoomNumber = tbx2f10.Text;
+            room.HostelType = rb1f10.Checked ? "Boy" : "Girl";
+
+            if (!int.TryParse(tbx3f10.Text, out int capacity) || capacity <= 0)
+            {
+                MessageBox.Show("Please enter valid Total Capacity!");
+                return;
+            }
+            room.TotalCapacity = capacity;
+
+            int occupancy = 0;
+            if (!string.IsNullOrEmpty(tbx4f10.Text))
+                int.TryParse(tbx4f10.Text, out occupancy);
+            room.CurrentOccupancy = occupancy;
+
+            room.RoomStatus = cbx1f10.Text;
+
+            string result = roomBL.SaveRoom(room);
+
+            if (result == "SUCCESS")
+            {
+                MessageBox.Show("Room Saved Successfully!");
+                ClearForm();
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Error: " + result);
+            }
+        }
+
+        // UPDATE Button
+        private void btnup_Click_1(object sender, EventArgs e)
+        {
+            if (dgvR.CurrentRow == null)
+            {
+                MessageBox.Show("Please click on a row to select a room first!");
+                return;
+            }
+
+            if (!ValidateForm())
+                return;
+
+            int roomID = Convert.ToInt32(dgvR.CurrentRow.Cells["RoomID"].Value);
+
+            RoomModel room = new RoomModel();
+            room.RoomID = roomID;
+            room.StudentName = tbx1f10.Text;
+            room.RoomNumber = tbx2f10.Text;
+            room.HostelType = rb1f10.Checked ? "Boy" : "Girl";
+
+            if (!int.TryParse(tbx3f10.Text, out int capacity) || capacity <= 0)
+            {
+                MessageBox.Show("Please enter valid Total Capacity!");
+                return;
+            }
+            room.TotalCapacity = capacity;
+
+            int occupancy = 0;
+            if (!string.IsNullOrEmpty(tbx4f10.Text))
+                int.TryParse(tbx4f10.Text, out occupancy);
+            room.CurrentOccupancy = occupancy;
+
+            room.RoomStatus = cbx1f10.Text;
+
+            string result = roomBL.UpdateRoom(room);
+
+            if (result == "SUCCESS")
+            {
+                MessageBox.Show("Room Updated Successfully!");
+                ClearForm();
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Error: " + result);
+            }
+        }
+
+        // DELETE Button
+        private void btndel_Click_1(object sender, EventArgs e)
+        {
+            if (dgvR.CurrentRow == null)
+            {
+                MessageBox.Show("Please click on a row to select a room first!");
+                return;
+            }
+
+            int roomID = Convert.ToInt32(dgvR.CurrentRow.Cells["RoomID"].Value);
+
+            DialogResult confirm = MessageBox.Show("Are you sure you want to delete this room?", "Confirm Delete",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (confirm == DialogResult.Yes)
+            {
+                string result = roomBL.DeleteRoom(roomID);
+
+                if (result == "SUCCESS")
+                {
+                    MessageBox.Show("Room Deleted Successfully!");
+                    ClearForm();
+                    LoadData();
+                }
+                else
+                {
+                    MessageBox.Show("Error: " + result);
+                }
+            }
+        }
+    }
+}
