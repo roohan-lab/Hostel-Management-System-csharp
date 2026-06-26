8a2525c3d0e4d6a038d1e3dbcd3e03974a68a910
roohan-lab <rohaanh616@gmail.com>
2026-06-26 16:56:01 +0500

    Add files via upload

---
+++ UI/AdminLogin.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace hostel_management_system_oop
+{
+    partial class AdminLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl2f2 = new System.Windows.Forms.Label();
+            this.lbl3f2 = new System.Windows.Forms.Label();
+            this.tbx1f2 = new System.Windows.Forms.TextBox();
+            this.tbx2f2 = new System.Windows.Forms.TextBox();
+            this.btn1f2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl2f2
+            // 
+            this.lbl2f2.AutoSize = true;
+            this.lbl2f2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl2f2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2f2.ForeColor = System.Drawing.Color.Black;
+            this.lbl2f2.Location = new System.Drawing.Point(246, 125);
+            this.lbl2f2.Name = "lbl2f2";
+            this.lbl2f2.Size = new System.Drawing.Size(106, 28);
+            this.lbl2f2.TabIndex = 1;
+            this.lbl2f2.Text = "Username";
+            // 
+            // lbl3f2
+            // 
+            this.lbl3f2.AutoSize = true;
+            this.lbl3f2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl3f2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3f2.ForeColor = System.Drawing.Color.Black;
+            this.lbl3f2.Location = new System.Drawing.Point(246, 184);
+            this.lbl3f2.Name = "lbl3f2";
+            this.lbl3f2.Size = new System.Drawing.Size(106, 28);
+            this.lbl3f2.TabIndex = 2;
+            this.lbl3f2.Text = "Password:";
+            // 
+            // tbx1f2
+            // 
+            this.tbx1f2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx1f2.Location = new System.Drawing.Point(383, 125);
+            this.tbx1f2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbx1f2.Name = "tbx1f2";
+            this.tbx1f2.Size = new System.Drawing.Size(209, 30);
+            this.tbx1f2.TabIndex = 3;
+            // 
+            // tbx2f2
+            // 
+            this.tbx2f2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx2f2.Location = new System.Drawing.Point(383, 182);
+            this.tbx2f2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbx2f2.Name = "tbx2f2";
+            this.tbx2f2.Size = new System.Drawing.Size(209, 30);
+            this.tbx2f2.TabIndex = 4;
+            // 
+            // btn1f2
+            // 
+            this.btn1f2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1f2.ForeColor = System.Drawing.Color.Black;
+            this.btn1f2.Location = new System.Drawing.Point(422, 252);
+            this.btn1f2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn1f2.Name = "btn1f2";
+            this.btn1f2.Size = new System.Drawing.Size(99, 40);
+            this.btn1f2.TabIndex = 5;
+            this.btn1f2.Text = "Log In";
+            this.btn1f2.UseVisualStyleBackColor = true;
+            this.btn1f2.Click += new System.EventHandler(this.btn1f2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(266, 403);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 23);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Did not registered yet?";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(492, 393);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 42);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(820, 84);
+            this.panel1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(270, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Admin Login";
+            // 
+            // AdminLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(823, 494);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn1f2);
+            this.Controls.Add(this.tbx2f2);
+            this.Controls.Add(this.tbx1f2);
+            this.Controls.Add(this.lbl3f2);
+            this.Controls.Add(this.lbl2f2);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "AdminLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Admin Login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl2f2;
+        private System.Windows.Forms.Label lbl3f2;
+        private System.Windows.Forms.TextBox tbx1f2;
+        private System.Windows.Forms.TextBox tbx2f2;
+        private System.Windows.Forms.Button btn1f2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ UI/AdminLogin.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+using hostel_management_system_oop.BL;
+using hostel_management_system_oop.UI;  // ← ADD THIS LINE
+
+namespace hostel_management_system_oop
+{
+    public partial class AdminLogin : BaseForm  // ← CHANGE Form TO BaseForm
+    {
+        // Static variable to store logged-in username
+        public static string LoggedInUsername = "";
+
+        private AdminBL adminBL = new AdminBL();
+
+        public AdminLogin()
+        {
+            InitializeComponent();
+            tbx2f2.PasswordChar = '*';
+            LoadData();  // ← ADD THIS LINE
+        }
+
+        // ← ADD THIS OVERRIDE METHOD
+        public override void LoadData()
+        {
+            tbx1f2.Clear();
+            tbx2f2.Clear();
+            tbx1f2.Focus();
+        }
+
+        // ← ADD THIS OVERRIDE METHOD
+        public override bool ValidateForm()
+        {
+            if (string.IsNullOrEmpty(tbx1f2.Text))
+            {
+                MessageBox.Show("Please enter Username!");
+                tbx1f2.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(tbx2f2.Text))
+            {
+                MessageBox.Show("Please enter Password!");
+                tbx2f2.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // The actual login code is in btn1f2_Click
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            SignUp signUp = new SignUp();
+            signUp.ShowDialog();
+        }
+
+        private void btn1f2_Click(object sender, EventArgs e)
+        {
+            // ← USING OVERRIDDEN ValidateForm
+            if (!ValidateForm())
+                return;
+
+            string username = tbx1f2.Text.Trim();
+            string password = tbx2f2.Text;
+
+            string result = adminBL.ValidateLogin(username, password);
+
+            if (result == "SUCCESS")
+            {
+                // Store the logged-in username
+                LoggedInUsername = username;
+
+                MessageBox.Show("Login Successful!");
+                Dashboard dashboard = new Dashboard();
+                dashboard.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show(result, "Login Failed");
+            }
+        }
+    }
+}
