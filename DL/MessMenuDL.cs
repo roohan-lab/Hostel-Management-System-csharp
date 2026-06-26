924980bafffebe02971d0214a84f360c27b9bc3b
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:15:08 +0500

    Add files via upload

---
+++ DL/MessMenuDL.cs
@@ -0,0 +1,61 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using hostel_management_system_oop.Model;
+
+namespace hostel_management_system_oop.DL
+{
+    public class MessMenuDL
+    {
+        public DataTable GetAllMenu()
+        {
+            string query = "SELECT MenuID, DayName, MealType, FoodItem FROM messmenu ORDER BY MenuID DESC";
+            return DatabaseConnection.ExecuteQuery(query);
+        }
+
+        public DataTable GetMenuById(int menuID)
+        {
+            string query = "SELECT * FROM messmenu WHERE MenuID = @id";
+            MySqlParameter[] parameters = { new MySqlParameter("@id", menuID) };
+            return DatabaseConnection.ExecuteQuery(query, parameters);
+        }
+
+        public bool InsertMenu(MessMenuModel menu)
+        {
+            string query = @"INSERT INTO messmenu (DayName, MealType, FoodItem) 
+                             VALUES (@day, @meal, @food)";
+
+            MySqlParameter[] parameters = {
+                new MySqlParameter("@day", menu.DayName),
+                new MySqlParameter("@meal", menu.MealType),
+                new MySqlParameter("@food", menu.FoodItem)
+            };
+
+            return DatabaseConnection.ExecuteNonQuery(query, parameters) > 0;
+        }
+
+        public bool UpdateMenu(MessMenuModel menu)
+        {
+            string query = @"UPDATE messmenu SET 
+                             DayName = @day,
+                             MealType = @meal,
+                             FoodItem = @food
+                             WHERE MenuID = @id";
+
+            MySqlParameter[] parameters = {
+                new MySqlParameter("@day", menu.DayName),
+                new MySqlParameter("@meal", menu.MealType),
+                new MySqlParameter("@food", menu.FoodItem),
+                new MySqlParameter("@id", menu.MenuID)
+            };
+
+            return DatabaseConnection.ExecuteNonQuery(query, parameters) > 0;
+        }
+
+        public bool DeleteMenu(int menuID)
+        {
+            string query = "DELETE FROM messmenu WHERE MenuID = @id";
+            MySqlParameter[] parameters = { new MySqlParameter("@id", menuID) };
+            return DatabaseConnection.ExecuteNonQuery(query, parameters) > 0;
+        }
+    }
+}
---
+++ DL/OwnerDL.cs
@@ -0,0 +1,35 @@
+﻿using hostel_management_system_oop.Model;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace hostel_management_system_oop.DL
+{
+    public class OwnerProfileDL
+    {
+        public DataTable GetOwnerByUsername(string username)
+        {
+            string query = "SELECT * FROM owners WHERE Username = @user";
+            MySqlParameter[] parameters = { new MySqlParameter("@user", username) };
+            return DatabaseConnection.ExecuteQuery(query, parameters);
+        }
+
+        public bool UpdateOwner(Owner owner)
+        {
+            string query = @"UPDATE owners SET 
+                             FullName = @name,
+                             Password = @pass,
+                             Email = @email,
+                             PhoneNumber = @phone
+                             WHERE Username = @user";
+
+            MySqlParameter[] parameters = {
+                new MySqlParameter("@name", owner.FullName),
+                new MySqlParameter("@pass", owner.Password),
+                new MySqlParameter("@email", owner.Email),
+                new MySqlParameter("@phone", owner.PhoneNumber),
+                new MySqlParameter("@user", owner.Username)
+            };
+            return DatabaseConnection.ExecuteNonQuery(query, parameters) > 0;
+        }
+    }
+}
