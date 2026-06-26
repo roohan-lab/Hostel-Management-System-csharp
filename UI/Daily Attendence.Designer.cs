a3b83823741c077d2f4e2f37769a23d755d2ca83
roohan-lab <rohaanh616@gmail.com>
2026-06-26 16:58:43 +0500

    Add files via upload

---
+++ UI/Daily Attendence.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace hostel_management_system_oop
+{
+    partial class DailyAttendence
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtp1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbx1f5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rbnpresent = new System.Windows.Forms.RadioButton();
+            this.rbnabsent = new System.Windows.Forms.RadioButton();
+            this.btn1f5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.btnup = new System.Windows.Forms.Button();
+            this.btnd = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(530, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Attendence Date:";
+            // 
+            // dtp1
+            // 
+            this.dtp1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtp1.Location = new System.Drawing.Point(787, 183);
+            this.dtp1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtp1.Name = "dtp1";
+            this.dtp1.Size = new System.Drawing.Size(385, 34);
+            this.dtp1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(639, 242);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Roll No:";
+            // 
+            // tbx1f5
+            // 
+            this.tbx1f5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx1f5.Location = new System.Drawing.Point(790, 242);
+            this.tbx1f5.Margin = new System.Windows.Forms.Padding(4);
+            this.tbx1f5.Name = "tbx1f5";
+            this.tbx1f5.Size = new System.Drawing.Size(383, 34);
+            this.tbx1f5.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(654, 306);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Status:";
+            // 
+            // rbnpresent
+            // 
+            this.rbnpresent.AutoSize = true;
+            this.rbnpresent.BackColor = System.Drawing.Color.White;
+            this.rbnpresent.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbnpresent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.rbnpresent.Location = new System.Drawing.Point(791, 306);
+            this.rbnpresent.Margin = new System.Windows.Forms.Padding(4);
+            this.rbnpresent.Name = "rbnpresent";
+            this.rbnpresent.Size = new System.Drawing.Size(104, 32);
+            this.rbnpresent.TabIndex = 6;
+            this.rbnpresent.TabStop = true;
+            this.rbnpresent.Text = "Present";
+            this.rbnpresent.UseVisualStyleBackColor = false;
+            // 
+            // rbnabsent
+            // 
+            this.rbnabsent.AutoSize = true;
+            this.rbnabsent.BackColor = System.Drawing.Color.White;
+            this.rbnabsent.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbnabsent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.rbnabsent.Location = new System.Drawing.Point(952, 306);
+            this.rbnabsent.Margin = new System.Windows.Forms.Padding(4);
+            this.rbnabsent.Name = "rbnabsent";
+            this.rbnabsent.Size = new System.Drawing.Size(99, 32);
+            this.rbnabsent.TabIndex = 7;
+            this.rbnabsent.TabStop = true;
+            this.rbnabsent.Text = "Absent";
+            this.rbnabsent.UseVisualStyleBackColor = false;
+            // 
+            // btn1f5
+            // 
+            this.btn1f5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1f5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btn1f5.Location = new System.Drawing.Point(790, 402);
+            this.btn1f5.Margin = new System.Windows.Forms.Padding(4);
+            this.btn1f5.Name = "btn1f5";
+            this.btn1f5.Size = new System.Drawing.Size(89, 42);
+            this.btn1f5.TabIndex = 8;
+            this.btn1f5.Text = "Save ";
+            this.btn1f5.UseVisualStyleBackColor = true;
+            this.btn1f5.Click += new System.EventHandler(this.btn1f5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(-9, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(2061, 107);
+            this.panel1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(689, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(371, 59);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Daily Attendence";
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(612, 496);
+            this.dgv1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowHeadersWidth = 52;
+            this.dgv1.RowTemplate.Height = 24;
+            this.dgv1.Size = new System.Drawing.Size(593, 244);
+            this.dgv1.TabIndex = 10;
+            // 
+            // btnup
+            // 
+            this.btnup.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnup.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnup.Location = new System.Drawing.Point(926, 402);
+            this.btnup.Margin = new System.Windows.Forms.Padding(4);
+            this.btnup.Name = "btnup";
+            this.btnup.Size = new System.Drawing.Size(102, 42);
+            this.btnup.TabIndex = 11;
+            this.btnup.Text = "Update";
+            this.btnup.UseVisualStyleBackColor = true;
+            this.btnup.Click += new System.EventHandler(this.btnup_Click);
+            // 
+            // btnd
+            // 
+            this.btnd.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnd.Location = new System.Drawing.Point(1071, 402);
+            this.btnd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnd.Name = "btnd";
+            this.btnd.Size = new System.Drawing.Size(102, 42);
+            this.btnd.TabIndex = 12;
+            this.btnd.Text = "Delete";
+            this.btnd.UseVisualStyleBackColor = true;
+            this.btnd.Click += new System.EventHandler(this.btnd_Click);
+            // 
+            // DailyAttendence
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(1540, 954);
+            this.Controls.Add(this.btnd);
+            this.Controls.Add(this.btnup);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn1f5);
+            this.Controls.Add(this.rbnabsent);
+            this.Controls.Add(this.rbnpresent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbx1f5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtp1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "DailyAttendence";
+            this.Text = "Form5";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtp1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbx1f5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rbnpresent;
+        private System.Windows.Forms.RadioButton rbnabsent;
+        private System.Windows.Forms.Button btn1f5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Button btnup;
+        private System.Windows.Forms.Button btnd;
+    }
+}
---
+++ UI/Daily Attendence.cs
@@ -0,0 +1,227 @@
+﻿using hostel_management_system_oop.BL;
+using hostel_management_system_oop.DL;
+using hostel_management_system_oop.Model;
+using hostel_management_system_oop.UI;  // ← ADD THIS LINE
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace hostel_management_system_oop
+{
+    public partial class DailyAttendence : BaseForm
+    {
+        private DailyAttendenceBL attendanceBL = new DailyAttendenceBL();
+        private int selectedAttendanceID = -1;
+
+        public DailyAttendence()
+        {
+            InitializeComponent();
+            SetupDataGridView();
+            dtp1.Value = DateTime.Now;
+            LoadData(); 
+        }
+
+      
+        public override void LoadData()
+        {
+            try
+            {
+                DataTable dt = attendanceBL.GetAllAttendance();
+                dgv1.Rows.Clear();
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    dgv1.Rows.Add(
+                        row["AttendanceID"].ToString(),
+                        row["RollNumber"].ToString(),
+                        Convert.ToDateTime(row["AttendanceDate"]).ToString("dd/MM/yyyy"),
+                        row["AttendanceStatus"].ToString()
+                    );
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading data: " + ex.Message);
+            }
+        }
+
+        // ← ADD THIS OVERRIDE METHOD (Replaces ClearForm)
+        public override void ClearForm()
+        {
+            tbx1f5.Clear();
+            dtp1.Value = DateTime.Now;
+            rbnpresent.Checked = false;
+            rbnabsent.Checked = false;
+            tbx1f5.Focus();
+            selectedAttendanceID = -1;
+        }
+
+       
+        public override bool ValidateForm()
+        {
+            if (string.IsNullOrEmpty(tbx1f5.Text))
+            {
+                MessageBox.Show("Please enter Roll Number!");
+                tbx1f5.Focus();
+                return false;
+            }
+
+            if (!rbnpresent.Checked && !rbnabsent.Checked)
+            {
+                MessageBox.Show("Please select Present or Absent!");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void SetupDataGridView()
+        {
+            dgv1.AutoGenerateColumns = false;
+            dgv1.Columns.Clear();
+
+            dgv1.Columns.Add("AttendanceID", "ID");
+            dgv1.Columns.Add("RollNumber", "Roll No");
+            dgv1.Columns.Add("AttendanceDate", "Date");
+            dgv1.Columns.Add("AttendanceStatus", "Status");
+
+            dgv1.Columns["AttendanceID"].Width = 60;
+            dgv1.Columns["RollNumber"].Width = 120;
+            dgv1.Columns["AttendanceDate"].Width = 130;
+            dgv1.Columns["AttendanceStatus"].Width = 100;
+
+            dgv1.AllowUserToAddRows = false;
+            dgv1.ReadOnly = true;
+            dgv1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv1.RowHeadersVisible = false;
+
+            dgv1.CellClick += dgv1_CellClick;
+        }
+
+        private void dgv1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgv1.Rows[e.RowIndex];
+
+                selectedAttendanceID = Convert.ToInt32(row.Cells["AttendanceID"].Value);
+
+                tbx1f5.Text = row.Cells["RollNumber"].Value.ToString();
+                dtp1.Value = Convert.ToDateTime(row.Cells["AttendanceDate"].Value);
+
+                string status = row.Cells["AttendanceStatus"].Value.ToString();
+                if (status == "Present")
+                    rbnpresent.Checked = true;
+                else if (status == "Absent")
+                    rbnabsent.Checked = true;
+            }
+        }
+
+        // SAVE Button
+        private void btn1f5_Click(object sender, EventArgs e)
+        {
+           
+            if (!ValidateForm())
+                return;
+
+            string rollNo = tbx1f5.Text;
+            DateTime attendanceDate = dtp1.Value.Date;
+            string status = "";
+
+            if (rbnpresent.Checked)
+                status = "Present";
+            else if (rbnabsent.Checked)
+                status = "Absent";
+
+            DailyAttendenceModel attendance = new DailyAttendenceModel();
+            attendance.RollNumber = rollNo;
+            attendance.AttendanceDate = attendanceDate;
+            attendance.AttendanceStatus = status;
+
+            string result = attendanceBL.SaveAttendance(attendance);
+
+            if (result == "SUCCESS")
+            {
+                MessageBox.Show("Attendance Saved Successfully!");
+                ClearForm(); 
+                LoadData();  
+            }
+            else
+            {
+                MessageBox.Show("Error: " + result);
+            }
+        }
+
+        // UPDATE Button
+        private void btnup_Click(object sender, EventArgs e)
+        {
+            if (selectedAttendanceID == -1)
+            {
+                MessageBox.Show("Please click on a row to select an attendance record first!");
+                return;
+            }
+
+            
+            if (!ValidateForm())
+                return;
+
+            string rollNo = tbx1f5.Text;
+            DateTime attendanceDate = dtp1.Value.Date;
+            string status = "";
+
+            if (rbnpresent.Checked)
+                status = "Present";
+            else if (rbnabsent.Checked)
+                status = "Absent";
+
+            DailyAttendenceModel attendance = new DailyAttendenceModel();
+            attendance.AttendanceID = selectedAttendanceID;
+            attendance.RollNumber = rollNo;
+            attendance.AttendanceDate = attendanceDate;
+            attendance.AttendanceStatus = status;
+
+            string result = attendanceBL.UpdateAttendance(attendance);
+
+            if (result == "SUCCESS")
+            {
+                MessageBox.Show("Attendance Updated Successfully!");
+                ClearForm();
+                LoadData();
+                selectedAttendanceID = -1;
+            }
+            else
+            {
+                MessageBox.Show("Error: " + result);
+            }
+        }
+
+        // DELETE Button
+        private void btnd_Click(object sender, EventArgs e)
+        {
+            if (selectedAttendanceID == -1)
+            {
+                MessageBox.Show("Please click on a row to select an attendance record first!");
+                return;
+            }
+
+            DialogResult confirm = MessageBox.Show("Are you sure you want to delete this attendance record?", "Confirm Delete",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (confirm == DialogResult.Yes)
+            {
+                string result = attendanceBL.DeleteAttendance(selectedAttendanceID);
+
+                if (result == "SUCCESS")
+                {
+                    MessageBox.Show("Attendance Deleted Successfully!");
+                    ClearForm();
+                    LoadData();
+                }
+                else
+                {
+                    MessageBox.Show("Error: " + result);
+                }
+            }
+        }
+    }
+}
