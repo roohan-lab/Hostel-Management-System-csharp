6d0d4639e6f80d9db7234479b4b5ca5cac0da4c9
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:01:54 +0500

    Add files via upload

---
+++ UI/Mess.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace hostel_management_system_oop
+{
+    partial class Mess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl1f4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbx1f7 = new System.Windows.Forms.TextBox();
+            this.tbx2f7 = new System.Windows.Forms.TextBox();
+            this.btn1f7 = new System.Windows.Forms.Button();
+            this.cbx1f7 = new System.Windows.Forms.ComboBox();
+            this.tbx4f7 = new System.Windows.Forms.TextBox();
+            this.tbxrf7 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvf = new System.Windows.Forms.DataGridView();
+            this.btn2f7 = new System.Windows.Forms.Button();
+            this.btn3f7 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvf)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.lbl1f4);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(2133, 110);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbl1f4
+            // 
+            this.lbl1f4.AutoSize = true;
+            this.lbl1f4.Font = new System.Drawing.Font("Segoe UI", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1f4.Location = new System.Drawing.Point(728, 31);
+            this.lbl1f4.Name = "lbl1f4";
+            this.lbl1f4.Size = new System.Drawing.Size(282, 59);
+            this.lbl1f4.TabIndex = 4;
+            this.lbl1f4.Text = "Mess Facility";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(627, 175);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hall Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(553, 236);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Seating Capacity:";
+            // 
+            // tbx1f7
+            // 
+            this.tbx1f7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx1f7.Location = new System.Drawing.Point(829, 170);
+            this.tbx1f7.Margin = new System.Windows.Forms.Padding(4);
+            this.tbx1f7.Name = "tbx1f7";
+            this.tbx1f7.Size = new System.Drawing.Size(349, 34);
+            this.tbx1f7.TabIndex = 3;
+            // 
+            // tbx2f7
+            // 
+            this.tbx2f7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx2f7.Location = new System.Drawing.Point(829, 231);
+            this.tbx2f7.Margin = new System.Windows.Forms.Padding(4);
+            this.tbx2f7.Name = "tbx2f7";
+            this.tbx2f7.Size = new System.Drawing.Size(349, 34);
+            this.tbx2f7.TabIndex = 4;
+            // 
+            // btn1f7
+            // 
+            this.btn1f7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1f7.Location = new System.Drawing.Point(829, 479);
+            this.btn1f7.Margin = new System.Windows.Forms.Padding(4);
+            this.btn1f7.Name = "btn1f7";
+            this.btn1f7.Size = new System.Drawing.Size(100, 43);
+            this.btn1f7.TabIndex = 5;
+            this.btn1f7.Text = "Save";
+            this.btn1f7.UseVisualStyleBackColor = true;
+            this.btn1f7.Click += new System.EventHandler(this.btn1f7_Click_1);
+            // 
+            // cbx1f7
+            // 
+            this.cbx1f7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx1f7.FormattingEnabled = true;
+            this.cbx1f7.Items.AddRange(new object[] {
+            "Computer Science",
+            "Cybersecuarity",
+            "Data Science",
+            "Electrical Engineering",
+            "Chemical Engineering",
+            "Textile Engineering"});
+            this.cbx1f7.Location = new System.Drawing.Point(829, 396);
+            this.cbx1f7.Margin = new System.Windows.Forms.Padding(4);
+            this.cbx1f7.Name = "cbx1f7";
+            this.cbx1f7.Size = new System.Drawing.Size(349, 36);
+            this.cbx1f7.TabIndex = 15;
+            // 
+            // tbx4f7
+            // 
+            this.tbx4f7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx4f7.Location = new System.Drawing.Point(829, 340);
+            this.tbx4f7.Margin = new System.Windows.Forms.Padding(4);
+            this.tbx4f7.Name = "tbx4f7";
+            this.tbx4f7.Size = new System.Drawing.Size(349, 34);
+            this.tbx4f7.TabIndex = 14;
+            // 
+            // tbxrf7
+            // 
+            this.tbxrf7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxrf7.Location = new System.Drawing.Point(829, 287);
+            this.tbxrf7.Margin = new System.Windows.Forms.Padding(4);
+            this.tbxrf7.Name = "tbxrf7";
+            this.tbxrf7.Size = new System.Drawing.Size(349, 34);
+            this.tbxrf7.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(605, 401);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 31);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Department:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(631, 340);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 31);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Full Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(660, 292);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 31);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Roll No:";
+            // 
+            // dgvf
+            // 
+            this.dgvf.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvf.Location = new System.Drawing.Point(199, 588);
+            this.dgvf.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvf.Name = "dgvf";
+            this.dgvf.RowHeadersWidth = 51;
+            this.dgvf.RowTemplate.Height = 24;
+            this.dgvf.Size = new System.Drawing.Size(1403, 306);
+            this.dgvf.TabIndex = 16;
+            // 
+            // btn2f7
+            // 
+            this.btn2f7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2f7.Location = new System.Drawing.Point(959, 479);
+            this.btn2f7.Margin = new System.Windows.Forms.Padding(4);
+            this.btn2f7.Name = "btn2f7";
+            this.btn2f7.Size = new System.Drawing.Size(100, 43);
+            this.btn2f7.TabIndex = 17;
+            this.btn2f7.Text = "Update";
+            this.btn2f7.UseVisualStyleBackColor = true;
+            this.btn2f7.Click += new System.EventHandler(this.btn2f7_Click_1);
+            // 
+            // btn3f7
+            // 
+            this.btn3f7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3f7.Location = new System.Drawing.Point(1080, 479);
+            this.btn3f7.Margin = new System.Windows.Forms.Padding(4);
+            this.btn3f7.Name = "btn3f7";
+            this.btn3f7.Size = new System.Drawing.Size(100, 43);
+            this.btn3f7.TabIndex = 18;
+            this.btn3f7.Text = "Delete";
+            this.btn3f7.UseVisualStyleBackColor = true;
+            this.btn3f7.Click += new System.EventHandler(this.btn3f7_Click_1);
+            // 
+            // Mess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(1892, 1040);
+            this.Controls.Add(this.btn3f7);
+            this.Controls.Add(this.btn2f7);
+            this.Controls.Add(this.dgvf);
+            this.Controls.Add(this.cbx1f7);
+            this.Controls.Add(this.tbx4f7);
+            this.Controls.Add(this.tbxrf7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn1f7);
+            this.Controls.Add(this.tbx2f7);
+            this.Controls.Add(this.tbx1f7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Mess";
+            this.Text = "Form7";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvf)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl1f4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbx1f7;
+        private System.Windows.Forms.TextBox tbx2f7;
+        private System.Windows.Forms.Button btn1f7;
+        private System.Windows.Forms.ComboBox cbx1f7;
+        private System.Windows.Forms.TextBox tbx4f7;
+        private System.Windows.Forms.TextBox tbxrf7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvf;
+        private System.Windows.Forms.Button btn2f7;
+        private System.Windows.Forms.Button btn3f7;
+    }
+}
---
+++ UI/Mess.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using hostel_management_system_oop.BL;
+using hostel_management_system_oop.UI;  // ← ADD THIS LINE
+
+namespace hostel_management_system_oop
+{
+    public partial class Mess : BaseForm  // ← CHANGE Form TO BaseForm
+    {
+        private MessBL messBL = new MessBL();
+        private string selectedHallName = "";
+        private string selectedRollNo = "";
+        private string selectedFullName = "";
+
+        public Mess()
+        {
+            InitializeComponent();
+            SetupDataGridView();
+            LoadDepartments();
+            LoadData(); 
+        }
+
+        
+        public override void LoadData()
+        {
+            try
+            {
+                DataTable dt = messBL.GetAllMessData();
+                dgvf.Rows.Clear();
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    dgvf.Rows.Add(
+                        row["HallName"].ToString(),
+                        row["TotalSittingCapacity"].ToString(),
+                        row["RollNumber"].ToString(),
+                        row["FullName"].ToString(),
+                        row["Department"].ToString()
+                    );
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading data: " + ex.Message);
+            }
+        }
+
+       
+        public override void ClearForm()
+        {
+            tbx1f7.Clear();
+            tbx2f7.Clear();
+            tbxrf7.Clear();
+            tbx4f7.Clear();
+            cbx1f7.SelectedIndex = 0;
+            tbx1f7.Focus();
+            selectedHallName = "";
+            selectedRollNo = "";
+            selectedFullName = "";
+        }
+
+       
+        public override bool ValidateForm()
+        {
+            if (string.IsNullOrEmpty(tbx1f7.Text))
+            {
+                MessageBox.Show("Please enter Hall Name!");
+                tbx1f7.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(tbx2f7.Text))
+            {
+                MessageBox.Show("Please enter Seating Capacity!");
+                tbx2f7.Focus();
+                return false;
+            }
+
+            int capacity;
+            if (!int.TryParse(tbx2f7.Text, out capacity) || capacity <= 0)
+            {
+                MessageBox.Show("Please enter valid Capacity!");
+                tbx2f7.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(tbxrf7.Text))
+            {
+                MessageBox.Show("Please enter Roll Number!");
+                tbxrf7.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(tbx4f7.Text))
+            {
+                MessageBox.Show("Please enter Full Name!");
+                tbx4f7.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void SetupDataGridView()
+        {
+            dgvf.AutoGenerateColumns = false;
+            dgvf.Columns.Clear();
+
+            dgvf.Columns.Add("HallName", "Hall Name");
+            dgvf.Columns.Add("TotalSittingCapacity", "Capacity");
+            dgvf.Columns.Add("RollNumber", "Roll No");
+            dgvf.Columns.Add("FullName", "Full Name");
+            dgvf.Columns.Add("Department", "Department");
+
+            dgvf.Columns["HallName"].Width = 120;
+            dgvf.Columns["TotalSittingCapacity"].Width = 70;
+            dgvf.Columns["RollNumber"].Width = 90;
+            dgvf.Columns["FullName"].Width = 130;
+            dgvf.Columns["Department"].Width = 160;
+
+            dgvf.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvf.AllowUserToAddRows = false;
+            dgvf.ReadOnly = true;
+            dgvf.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvf.RowHeadersVisible = false;
+
+            dgvf.CellClick += dgvf_CellClick;
+        }
+
+        private void LoadDepartments()
+        {
+            cbx1f7.Items.Clear();
+            cbx1f7.Items.Add("Computer Science (CS)");
+            cbx1f7.Items.Add("Electrical Engineering (EE)");
+            cbx1f7.Items.Add("Mechanical Engineering (ME)");
+            cbx1f7.Items.Add("Civil Engineering (CE)");
+            cbx1f7.Items.Add("Software Engineering (SE)");
+            cbx1f7.Items.Add("Information Technology (IT)");
+            cbx1f7.Items.Add("Business Administration (BBA)");
+            cbx1f7.SelectedIndex = 0;
+        }
+
+        private void dgvf_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvf.Rows[e.RowIndex];
+
+                selectedHallName = row.Cells["HallName"].Value.ToString();
+                selectedRollNo = row.Cells["RollNumber"].Value.ToString();
+                selectedFullName = row.Cells["FullName"].Value.ToString();
+
+                tbx1f7.Text = row.Cells["HallName"].Value.ToString();
+                tbx2f7.Text = row.Cells["TotalSittingCapacity"].Value.ToString();
+                tbxrf7.Text = row.Cells["RollNumber"].Value.ToString();
+                tbx4f7.Text = row.Cells["FullName"].Value.ToString();
+                cbx1f7.Text = row.Cells["Department"].Value.ToString();
+            }
+        }
+
+       
+        private void btn1f7_Click_1(object sender, EventArgs e)
+        {
+           
+            if (!ValidateForm())
+                return;
+
+            string hallName = tbx1f7.Text;
+            string capacity = tbx2f7.Text;
+            string rollNo = tbxrf7.Text;
+            string fullName = tbx4f7.Text;
+            string department = cbx1f7.Text;
+
+            int cap;
+            if (!int.TryParse(capacity, out cap) || cap <= 0)
+            {
+                MessageBox.Show("Please enter valid Capacity!");
+                return;
+            }
+
+            string result = messBL.SaveMessData(hallName, cap, rollNo, fullName, department);
+
+            if (result == "SUCCESS")
+            {
+                MessageBox.Show("Data Saved Successfully!");
+                ClearForm(); 
+                LoadData();  
+            }
+            else
+            {
+                MessageBox.Show("Error: " + result);
+            }
+        }
+
+        // UPDATE Button
+        private void btn2f7_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedHallName) || string.IsNullOrEmpty(selectedRollNo))
+            {
+                MessageBox.Show("Please click on a row to select a record first!");
+                return;
+            }
+
+            
+            if (!ValidateForm())
+                return;
+
+            string hallName = tbx1f7.Text;
+            string capacity = tbx2f7.Text;
+            string rollNo = tbxrf7.Text;
+            string fullName = tbx4f7.Text;
+            string department = cbx1f7.Text;
+
+            int cap;
+            if (!int.TryParse(capacity, out cap) || cap <= 0)
+            {
+                MessageBox.Show("Please enter valid Capacity!");
+                return;
+            }
+
+            string result = messBL.UpdateMessData(selectedHallName, selectedRollNo, selectedFullName,
+                                                  hallName, cap, rollNo, fullName, department);
+
+            if (result == "SUCCESS")
+            {
+                MessageBox.Show("Data Updated Successfully!");
+                ClearForm();
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Error: " + result);
+            }
+        }
+
+       
+        private void btn3f7_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedHallName) || string.IsNullOrEmpty(selectedRollNo))
+            {
+                MessageBox.Show("Please click on a row to select a record first!");
+                return;
+            }
+
+            DialogResult confirm = MessageBox.Show("Are you sure you want to delete this record?\n\n" +
+                "Hall: " + selectedHallName + "\nRoll No: " + selectedRollNo + "\nName: " + selectedFullName,
+                "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (confirm == DialogResult.Yes)
+            {
+                string result = messBL.DeleteMessData(selectedHallName, selectedRollNo, selectedFullName);
+
+                if (result == "SUCCESS")
+                {
+                    MessageBox.Show("Data Deleted Successfully!");
+                    ClearForm();
+                    LoadData();
+                }
+                else
+                {
+                    MessageBox.Show("Error: " + result);
+                }
+            }
+        }
+    }
+}
