2ab78df1a4a5e2fb9614e799bfd1e046eed3fa62
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:25:50 +0500

    Add files via upload

---
+++ Model/Owner.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+namespace hostel_management_system_oop.Model
+
+{
+    public class Owner
+    {
+        public int OwnerID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
