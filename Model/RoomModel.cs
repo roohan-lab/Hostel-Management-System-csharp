d4a5f715ea98cc674b8caae7ed97b6455f435cba
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:25:29 +0500

    Add files via upload

---
+++ Model/RoomModel.cs
@@ -0,0 +1,13 @@
+﻿namespace hostel_management_system_oop.Model
+{
+    public class RoomModel
+    {
+        public int RoomID { get; set; }
+        public string StudentName { get; set; }  // Add this
+        public string RoomNumber { get; set; }
+        public string HostelType { get; set; }
+        public int TotalCapacity { get; set; }
+        public int CurrentOccupancy { get; set; }
+        public string RoomStatus { get; set; }
+    }
+}
