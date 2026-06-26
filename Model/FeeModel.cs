6482dcdb5104a1acc3e12106511234625bb1b5cd
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:24:01 +0500

    Add files via upload

---
+++ Model/FeeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hostel_management_system_oop.Model
+{
+    public class FeeModel
+    {
+        public int FeeID { get; set; }
+        public string RollNumber { get; set; }
+        public int AmountDue { get; set; }
+        public int AmountPaid { get; set; }
+        public DateTime DueDate { get; set; }
+        public string PaymentStatus { get; set; }
+    }
+}
