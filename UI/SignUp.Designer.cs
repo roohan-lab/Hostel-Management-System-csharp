a9cc89da6de0b7728beee40bdb9274e39068e3c2
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:05:15 +0500

    Add files via upload

---
+++ UI/SignUp.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace hostel_management_system_oop
+{
+    partial class SignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1R = new System.Windows.Forms.Button();
+            this.tbx2R = new System.Windows.Forms.TextBox();
+            this.tbx1R = new System.Windows.Forms.TextBox();
+            this.lbl3f2 = new System.Windows.Forms.Label();
+            this.lbl2f2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1R
+            // 
+            this.btn1R.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1R.Location = new System.Drawing.Point(356, 261);
+            this.btn1R.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn1R.Name = "btn1R";
+            this.btn1R.Size = new System.Drawing.Size(131, 47);
+            this.btn1R.TabIndex = 14;
+            this.btn1R.Text = "Sign Up";
+            this.btn1R.UseVisualStyleBackColor = true;
+            this.btn1R.Click += new System.EventHandler(this.btn1R_Click_1);
+            // 
+            // tbx2R
+            // 
+            this.tbx2R.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx2R.Location = new System.Drawing.Point(335, 188);
+            this.tbx2R.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbx2R.Name = "tbx2R";
+            this.tbx2R.Size = new System.Drawing.Size(209, 30);
+            this.tbx2R.TabIndex = 13;
+            // 
+            // tbx1R
+            // 
+            this.tbx1R.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx1R.Location = new System.Drawing.Point(335, 132);
+            this.tbx1R.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbx1R.Name = "tbx1R";
+            this.tbx1R.Size = new System.Drawing.Size(209, 30);
+            this.tbx1R.TabIndex = 12;
+            // 
+            // lbl3f2
+            // 
+            this.lbl3f2.AutoSize = true;
+            this.lbl3f2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl3f2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3f2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbl3f2.Location = new System.Drawing.Point(235, 191);
+            this.lbl3f2.Name = "lbl3f2";
+            this.lbl3f2.Size = new System.Drawing.Size(86, 23);
+            this.lbl3f2.TabIndex = 11;
+            this.lbl3f2.Text = "Password:";
+            // 
+            // lbl2f2
+            // 
+            this.lbl2f2.AutoSize = true;
+            this.lbl2f2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl2f2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2f2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbl2f2.Location = new System.Drawing.Point(235, 132);
+            this.lbl2f2.Name = "lbl2f2";
+            this.lbl2f2.Size = new System.Drawing.Size(87, 23);
+            this.lbl2f2.TabIndex = 10;
+            this.lbl2f2.Text = "Username";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(820, 84);
+            this.panel1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(275, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registration Form";
+            // 
+            // SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(823, 436);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn1R);
+            this.Controls.Add(this.tbx2R);
+            this.Controls.Add(this.tbx1R);
+            this.Controls.Add(this.lbl3f2);
+            this.Controls.Add(this.lbl2f2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "SignUp";
+            this.Text = "SignUp";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn1R;
+        private System.Windows.Forms.TextBox tbx2R;
+        private System.Windows.Forms.TextBox tbx1R;
+        private System.Windows.Forms.Label lbl3f2;
+        private System.Windows.Forms.Label lbl2f2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ UI/SignUp.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows.Forms;
+using hostel_management_system_oop.BL;
+using hostel_management_system_oop.UI;  
+
+namespace hostel_management_system_oop
+{
+    public partial class SignUp : BaseForm  
+    {
+        private SignUpBL signUpBL = new SignUpBL();
+
+        public SignUp()
+        {
+            InitializeComponent();
+            LoadData(); 
+        }
+
+       
+        public override void LoadData()
+        {
+            tbx1R.Clear();
+            tbx2R.Clear();
+            tbx1R.Focus();
+        }
+
+       
+        public override void ClearForm()
+        {
+            tbx1R.Clear();
+            tbx2R.Clear();
+            tbx1R.Focus();
+        }
+
+       
+        public override bool ValidateForm()
+        {
+            string username = tbx1R.Text.Trim();
+            string password = tbx2R.Text;
+
+            if (string.IsNullOrEmpty(username))
+            {
+                MessageBox.Show("Please enter Username!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                tbx1R.Focus();
+                return false;
+            }
+
+            if (username.Length < 3)
+            {
+                MessageBox.Show("Username must be at least 3 characters!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                tbx1R.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter Password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                tbx2R.Focus();
+                return false;
+            }
+
+            if (password.Length < 4)
+            {
+                MessageBox.Show("Password must be at least 4 characters!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                tbx2R.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btn1R_Click(object sender, EventArgs e)
+        {
+           
+            if (!ValidateForm())
+                return;
+
+            string username = tbx1R.Text.Trim();
+            string password = tbx2R.Text;
+
+            string result = signUpBL.RegisterUser(username, password);
+
+            if (result == "SUCCESS")
+            {
+                MessageBox.Show("Registration Successful! You can now login.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearForm(); 
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show(result, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn1R_Click_1(object sender, EventArgs e)
+        {
+           
+            btn1R_Click(sender, e);
+        }
+    }
+}
