da63a3f61d984ca6687b5f7b392e8f19c262d127
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:05:43 +0500

    Add files via upload

---
+++ UI/StudentProfile.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace hostel_management_system_oop
+{
+    partial class StudentProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1f3 = new System.Windows.Forms.Label();
+            this.lbl2f3 = new System.Windows.Forms.Label();
+            this.lbl3f3 = new System.Windows.Forms.Label();
+            this.lbl4f3 = new System.Windows.Forms.Label();
+            this.lbl5f3 = new System.Windows.Forms.Label();
+            this.lbl6f3 = new System.Windows.Forms.Label();
+            this.lbl7f3 = new System.Windows.Forms.Label();
+            this.lbl8f3 = new System.Windows.Forms.Label();
+            this.tbx1f3 = new System.Windows.Forms.TextBox();
+            this.tbx2f3 = new System.Windows.Forms.TextBox();
+            this.tbx3f3 = new System.Windows.Forms.TextBox();
+            this.tbx5f3 = new System.Windows.Forms.TextBox();
+            this.tbx7f3 = new System.Windows.Forms.TextBox();
+            this.tbx4f3 = new System.Windows.Forms.TextBox();
+            this.tbx6f3 = new System.Windows.Forms.TextBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvst = new System.Windows.Forms.DataGridView();
+            this.btnup = new System.Windows.Forms.Button();
+            this.btndel = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvst)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl1f3
+            // 
+            this.lbl1f3.AutoSize = true;
+            this.lbl1f3.Font = new System.Drawing.Font("Segoe UI", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1f3.Location = new System.Drawing.Point(785, 20);
+            this.lbl1f3.Name = "lbl1f3";
+            this.lbl1f3.Size = new System.Drawing.Size(328, 59);
+            this.lbl1f3.TabIndex = 6;
+            this.lbl1f3.Text = "Student Record";
+            // 
+            // lbl2f3
+            // 
+            this.lbl2f3.AutoSize = true;
+            this.lbl2f3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl2f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2f3.Location = new System.Drawing.Point(665, 150);
+            this.lbl2f3.Name = "lbl2f3";
+            this.lbl2f3.Size = new System.Drawing.Size(118, 28);
+            this.lbl2f3.TabIndex = 7;
+            this.lbl2f3.Text = "Student ID:";
+            // 
+            // lbl3f3
+            // 
+            this.lbl3f3.AutoSize = true;
+            this.lbl3f3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl3f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3f3.Location = new System.Drawing.Point(695, 207);
+            this.lbl3f3.Name = "lbl3f3";
+            this.lbl3f3.Size = new System.Drawing.Size(88, 28);
+            this.lbl3f3.TabIndex = 8;
+            this.lbl3f3.Text = "Roll No:";
+            // 
+            // lbl4f3
+            // 
+            this.lbl4f3.AutoSize = true;
+            this.lbl4f3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl4f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4f3.Location = new System.Drawing.Point(670, 263);
+            this.lbl4f3.Name = "lbl4f3";
+            this.lbl4f3.Size = new System.Drawing.Size(113, 28);
+            this.lbl4f3.TabIndex = 9;
+            this.lbl4f3.Text = "Full Name:";
+            // 
+            // lbl5f3
+            // 
+            this.lbl5f3.AutoSize = true;
+            this.lbl5f3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl5f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5f3.Location = new System.Drawing.Point(722, 319);
+            this.lbl5f3.Name = "lbl5f3";
+            this.lbl5f3.Size = new System.Drawing.Size(63, 28);
+            this.lbl5f3.TabIndex = 10;
+            this.lbl5f3.Text = "CNIC:";
+            // 
+            // lbl6f3
+            // 
+            this.lbl6f3.AutoSize = true;
+            this.lbl6f3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl6f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl6f3.Location = new System.Drawing.Point(697, 377);
+            this.lbl6f3.Name = "lbl6f3";
+            this.lbl6f3.Size = new System.Drawing.Size(86, 28);
+            this.lbl6f3.TabIndex = 11;
+            this.lbl6f3.Text = "Gender:";
+            // 
+            // lbl7f3
+            // 
+            this.lbl7f3.AutoSize = true;
+            this.lbl7f3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl7f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl7f3.Location = new System.Drawing.Point(625, 426);
+            this.lbl7f3.Name = "lbl7f3";
+            this.lbl7f3.Size = new System.Drawing.Size(160, 28);
+            this.lbl7f3.TabIndex = 12;
+            this.lbl7f3.Text = "Phone Number:";
+            // 
+            // lbl8f3
+            // 
+            this.lbl8f3.AutoSize = true;
+            this.lbl8f3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbl8f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl8f3.Location = new System.Drawing.Point(716, 487);
+            this.lbl8f3.Name = "lbl8f3";
+            this.lbl8f3.Size = new System.Drawing.Size(69, 28);
+            this.lbl8f3.TabIndex = 13;
+            this.lbl8f3.Text = "Email:";
+            // 
+            // tbx1f3
+            // 
+            this.tbx1f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx1f3.Location = new System.Drawing.Point(841, 144);
+            this.tbx1f3.Name = "tbx1f3";
+            this.tbx1f3.Size = new System.Drawing.Size(392, 34);
+            this.tbx1f3.TabIndex = 14;
+            // 
+            // tbx2f3
+            // 
+            this.tbx2f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx2f3.Location = new System.Drawing.Point(841, 198);
+            this.tbx2f3.Name = "tbx2f3";
+            this.tbx2f3.Size = new System.Drawing.Size(392, 34);
+            this.tbx2f3.TabIndex = 15;
+            // 
+            // tbx3f3
+            // 
+            this.tbx3f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx3f3.Location = new System.Drawing.Point(841, 257);
+            this.tbx3f3.Name = "tbx3f3";
+            this.tbx3f3.Size = new System.Drawing.Size(392, 34);
+            this.tbx3f3.TabIndex = 16;
+            // 
+            // tbx5f3
+            // 
+            this.tbx5f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx5f3.Location = new System.Drawing.Point(840, 371);
+            this.tbx5f3.Name = "tbx5f3";
+            this.tbx5f3.Size = new System.Drawing.Size(393, 34);
+            this.tbx5f3.TabIndex = 17;
+            // 
+            // tbx7f3
+            // 
+            this.tbx7f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx7f3.Location = new System.Drawing.Point(840, 482);
+            this.tbx7f3.Name = "tbx7f3";
+            this.tbx7f3.Size = new System.Drawing.Size(393, 34);
+            this.tbx7f3.TabIndex = 18;
+            // 
+            // tbx4f3
+            // 
+            this.tbx4f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx4f3.Location = new System.Drawing.Point(840, 313);
+            this.tbx4f3.Name = "tbx4f3";
+            this.tbx4f3.Size = new System.Drawing.Size(393, 34);
+            this.tbx4f3.TabIndex = 19;
+            // 
+            // tbx6f3
+            // 
+            this.tbx6f3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx6f3.Location = new System.Drawing.Point(840, 426);
+            this.tbx6f3.Name = "tbx6f3";
+            this.tbx6f3.Size = new System.Drawing.Size(393, 34);
+            this.tbx6f3.TabIndex = 20;
+            // 
+            // btnsave
+            // 
+            this.btnsave.BackColor = System.Drawing.Color.White;
+            this.btnsave.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.Location = new System.Drawing.Point(988, 556);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(100, 41);
+            this.btnsave.TabIndex = 21;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = false;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.lbl1f3);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(2261, 112);
+            this.panel1.TabIndex = 22;
+            // 
+            // dgvst
+            // 
+            this.dgvst.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvst.Location = new System.Drawing.Point(585, 640);
+            this.dgvst.Name = "dgvst";
+            this.dgvst.RowHeadersWidth = 51;
+            this.dgvst.RowTemplate.Height = 24;
+            this.dgvst.Size = new System.Drawing.Size(804, 311);
+            this.dgvst.TabIndex = 23;
+            // 
+            // btnup
+            // 
+            this.btnup.BackColor = System.Drawing.Color.White;
+            this.btnup.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnup.Location = new System.Drawing.Point(841, 556);
+            this.btnup.Name = "btnup";
+            this.btnup.Size = new System.Drawing.Size(92, 41);
+            this.btnup.TabIndex = 24;
+            this.btnup.Text = "Update";
+            this.btnup.UseVisualStyleBackColor = false;
+            this.btnup.Click += new System.EventHandler(this.btnup_Click);
+            // 
+            // btndel
+            // 
+            this.btndel.BackColor = System.Drawing.Color.White;
+            this.btndel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndel.Location = new System.Drawing.Point(1145, 556);
+            this.btndel.Name = "btndel";
+            this.btndel.Size = new System.Drawing.Size(88, 41);
+            this.btndel.TabIndex = 25;
+            this.btndel.Text = "Delete";
+            this.btndel.UseVisualStyleBackColor = false;
+            this.btndel.Click += new System.EventHandler(this.btndel_Click);
+            // 
+            // StudentProfile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.btndel);
+            this.Controls.Add(this.btnup);
+            this.Controls.Add(this.dgvst);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.tbx6f3);
+            this.Controls.Add(this.tbx4f3);
+            this.Controls.Add(this.tbx7f3);
+            this.Controls.Add(this.tbx5f3);
+            this.Controls.Add(this.tbx3f3);
+            this.Controls.Add(this.tbx2f3);
+            this.Controls.Add(this.tbx1f3);
+            this.Controls.Add(this.lbl8f3);
+            this.Controls.Add(this.lbl7f3);
+            this.Controls.Add(this.lbl6f3);
+            this.Controls.Add(this.lbl5f3);
+            this.Controls.Add(this.lbl4f3);
+            this.Controls.Add(this.lbl3f3);
+            this.Controls.Add(this.lbl2f3);
+            this.Name = "StudentProfile";
+            this.Text = "Form3";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvst)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl1f3;
+        private System.Windows.Forms.Label lbl2f3;
+        private System.Windows.Forms.Label lbl3f3;
+        private System.Windows.Forms.Label lbl4f3;
+        private System.Windows.Forms.Label lbl5f3;
+        private System.Windows.Forms.Label lbl6f3;
+        private System.Windows.Forms.Label lbl7f3;
+        private System.Windows.Forms.Label lbl8f3;
+        private System.Windows.Forms.TextBox tbx1f3;
+        private System.Windows.Forms.TextBox tbx2f3;
+        private System.Windows.Forms.TextBox tbx3f3;
+        private System.Windows.Forms.TextBox tbx5f3;
+        private System.Windows.Forms.TextBox tbx7f3;
+        private System.Windows.Forms.TextBox tbx4f3;
+        private System.Windows.Forms.TextBox tbx6f3;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgvst;
+        private System.Windows.Forms.Button btnup;
+        private System.Windows.Forms.Button btndel;
+    }
+}
---
+++ UI/StudentProfile.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using hostel_management_system_oop.BL;
+using hostel_management_system_oop.Model;
+using hostel_management_system_oop.UI;
+
+namespace hostel_management_system_oop
+{
+    public partial class StudentProfile : AbstractBaseForm
+    {
+        private StudentBL studentBL = new StudentBL();
+        private int selectedStudentID = -1;
+
+        public StudentProfile()
+        {
+            // ✅ FIX: Check if NOT in design mode
+            if (!DesignMode)
+            {
+                InitializeComponent();
+                this.Text = FormTitle;
+                LoadData();
+                dgvst.SelectionChanged += dgvst_SelectionChanged;
+            }
+            else
+            {
+                InitializeComponent();
+            }
+        }
+
+        public override string FormTitle
+        {
+            get { return "Student Management"; }
+        }
+
+        public override void LoadData()
+        {
+            try
+            {
+                DataTable dt = studentBL.GetAllStudents();
+                dgvst.DataSource = dt;
+
+                // Set column headers
+                if (dgvst.Columns.Contains("StudentID"))
+                    dgvst.Columns["StudentID"].HeaderText = "ID";
+                if (dgvst.Columns.Contains("RollNumber"))
+                    dgvst.Columns["RollNumber"].HeaderText = "Roll No";
+                if (dgvst.Columns.Contains("FullName"))
+                    dgvst.Columns["FullName"].HeaderText = "Full Name";
+                if (dgvst.Columns.Contains("CNIC"))
+                    dgvst.Columns["CNIC"].HeaderText = "CNIC";
+                if (dgvst.Columns.Contains("Gender"))
+                    dgvst.Columns["Gender"].HeaderText = "Gender";
+                if (dgvst.Columns.Contains("PhoneNumber"))
+                    dgvst.Columns["PhoneNumber"].HeaderText = "Phone";
+                if (dgvst.Columns.Contains("Email"))
+                    dgvst.Columns["Email"].HeaderText = "Email";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading students: " + ex.Message);
+            }
+        }
+
+        public override void ClearForm()
+        {
+            tbx2f3.Clear();
+            tbx3f3.Clear();
+            tbx4f3.Clear();
+            tbx5f3.Clear();
+            tbx6f3.Clear();
+            tbx7f3.Clear();
+            tbx2f3.Focus();
+            selectedStudentID = -1;
+        }
+
+        public override bool ValidateForm()
+        {
+            if (string.IsNullOrEmpty(tbx2f3.Text))
+            {
+                MessageBox.Show("Please enter Roll Number!");
+                tbx2f3.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(tbx3f3.Text))
+            {
+                MessageBox.Show("Please enter Full Name!");
+                tbx3f3.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(tbx4f3.Text))
+            {
+                MessageBox.Show("Please enter CNIC!");
+                tbx4f3.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void dgvst_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvst.SelectedRows.Count > 0)
+            {
+                selectedStudentID = Convert.ToInt32(dgvst.SelectedRows[0].Cells["StudentID"].Value);
+
+                tbx2f3.Text = dgvst.SelectedRows[0].Cells["RollNumber"].Value.ToString();
+                tbx3f3.Text = dgvst.SelectedRows[0].Cells["FullName"].Value.ToString();
+                tbx4f3.Text = dgvst.SelectedRows[0].Cells["CNIC"].Value.ToString();
+                tbx5f3.Text = dgvst.SelectedRows[0].Cells["Gender"].Value.ToString();
+                tbx6f3.Text = dgvst.SelectedRows[0].Cells["PhoneNumber"].Value.ToString();
+                tbx7f3.Text = dgvst.SelectedRows[0].Cells["Email"].Value.ToString();
+            }
+        }
+
+        // SAVE Button
+        private void btnsave_Click_1(object sender, EventArgs e)
+        {
+            if (!ValidateForm())
+                return;
+
+            Student student = new Student();
+            student.RollNumber = tbx2f3.Text;
+            student.FullName = tbx3f3.Text;
+            student.CNIC = tbx4f3.Text;
+            student.Gender = tbx5f3.Text;
+            student.PhoneNumber = tbx6f3.Text;
+            student.Email = tbx7f3.Text;
+
+            string result = studentBL.SaveStudent(student);
+
+            if (result == "SUCCESS")
+            {
+                MessageBox.Show("Student Saved Successfully!");
+                ClearForm();
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Error: " + result);
+            }
+        }
+
+        // UPDATE Button
+        private void btnup_Click(object sender, EventArgs e)
+        {
+            if (selectedStudentID == -1)
+            {
+                MessageBox.Show("Please select a student from the list first!");
+                return;
+            }
+
+            if (!ValidateForm())
+                return;
+
+            Student student = new Student();
+            student.StudentID = selectedStudentID;
+            student.RollNumber = tbx2f3.Text;
+            student.FullName = tbx3f3.Text;
+            student.CNIC = tbx4f3.Text;
+            student.Gender = tbx5f3.Text;
+            student.PhoneNumber = tbx6f3.Text;
+            student.Email = tbx7f3.Text;
+
+            string result = studentBL.UpdateStudent(student);
+
+            if (result == "SUCCESS")
+            {
+                MessageBox.Show("Student Updated Successfully!");
+                ClearForm();
+                LoadData();
+                selectedStudentID = -1;
+            }
+            else
+            {
+                MessageBox.Show("Error: " + result);
+            }
+        }
+
+        // DELETE Button
+        private void btndel_Click(object sender, EventArgs e)
+        {
+            if (selectedStudentID == -1)
+            {
+                MessageBox.Show("Please select a student from the list first!");
+                return;
+            }
+
+            DialogResult confirm = MessageBox.Show("Are you sure you want to delete this student?", "Confirm Delete",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (confirm == DialogResult.Yes)
+            {
+                string result = studentBL.DeleteStudent(selectedStudentID);
+
+                if (result == "SUCCESS")
+                {
+                    MessageBox.Show("Student Deleted Successfully!");
+                    ClearForm();
+                    LoadData();
+                    selectedStudentID = -1;
+                }
+                else
+                {
+                    MessageBox.Show("Error: " + result);
+                }
+            }
+        }
+    }
+}
