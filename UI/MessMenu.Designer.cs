b04550d1edb78ba48e69901c6e3e1ee02c211ba8
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:02:56 +0500

    Add files via upload

---
+++ UI/MessMenu.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace hostel_management_system_oop
+{
+    partial class MessMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl1f4 = new System.Windows.Forms.Label();
+            this.cbx1f8 = new System.Windows.Forms.ComboBox();
+            this.rb1f8 = new System.Windows.Forms.RadioButton();
+            this.tbx1f8 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn1f8 = new System.Windows.Forms.Button();
+            this.rb2f8 = new System.Windows.Forms.RadioButton();
+            this.rb3f8 = new System.Windows.Forms.RadioButton();
+            this.dgvMenu = new System.Windows.Forms.DataGridView();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn2f8 = new System.Windows.Forms.Button();
+            this.btn3f8 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.lbl1f4);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(2259, 119);
+            this.panel1.TabIndex = 2;
+            // 
+            // lbl1f4
+            // 
+            this.lbl1f4.AutoSize = true;
+            this.lbl1f4.Font = new System.Drawing.Font("Segoe UI", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1f4.Location = new System.Drawing.Point(726, 24);
+            this.lbl1f4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl1f4.Name = "lbl1f4";
+            this.lbl1f4.Size = new System.Drawing.Size(245, 59);
+            this.lbl1f4.TabIndex = 4;
+            this.lbl1f4.Text = "Mess Menu";
+            // 
+            // cbx1f8
+            // 
+            this.cbx1f8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx1f8.FormattingEnabled = true;
+            this.cbx1f8.Items.AddRange(new object[] {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"});
+            this.cbx1f8.Location = new System.Drawing.Point(697, 173);
+            this.cbx1f8.Margin = new System.Windows.Forms.Padding(4);
+            this.cbx1f8.Name = "cbx1f8";
+            this.cbx1f8.Size = new System.Drawing.Size(346, 36);
+            this.cbx1f8.TabIndex = 3;
+            // 
+            // rb1f8
+            // 
+            this.rb1f8.AutoSize = true;
+            this.rb1f8.BackColor = System.Drawing.Color.White;
+            this.rb1f8.Location = new System.Drawing.Point(697, 241);
+            this.rb1f8.Margin = new System.Windows.Forms.Padding(4);
+            this.rb1f8.Name = "rb1f8";
+            this.rb1f8.Size = new System.Drawing.Size(100, 27);
+            this.rb1f8.TabIndex = 4;
+            this.rb1f8.TabStop = true;
+            this.rb1f8.Text = "Breakfast";
+            this.rb1f8.UseVisualStyleBackColor = false;
+            // 
+            // tbx1f8
+            // 
+            this.tbx1f8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx1f8.Location = new System.Drawing.Point(697, 309);
+            this.tbx1f8.Margin = new System.Windows.Forms.Padding(4);
+            this.tbx1f8.Name = "tbx1f8";
+            this.tbx1f8.Size = new System.Drawing.Size(352, 34);
+            this.tbx1f8.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(514, 174);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Day Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(514, 237);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 31);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Meal Type:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(513, 309);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 31);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Food Item:";
+            // 
+            // btn1f8
+            // 
+            this.btn1f8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1f8.Location = new System.Drawing.Point(697, 377);
+            this.btn1f8.Margin = new System.Windows.Forms.Padding(4);
+            this.btn1f8.Name = "btn1f8";
+            this.btn1f8.Size = new System.Drawing.Size(88, 47);
+            this.btn1f8.TabIndex = 9;
+            this.btn1f8.Text = "Update";
+            this.btn1f8.UseVisualStyleBackColor = true;
+            this.btn1f8.Click += new System.EventHandler(this.btn1f8_Click);
+            // 
+            // rb2f8
+            // 
+            this.rb2f8.AutoSize = true;
+            this.rb2f8.BackColor = System.Drawing.Color.White;
+            this.rb2f8.Location = new System.Drawing.Point(837, 240);
+            this.rb2f8.Name = "rb2f8";
+            this.rb2f8.Size = new System.Drawing.Size(77, 27);
+            this.rb2f8.TabIndex = 10;
+            this.rb2f8.TabStop = true;
+            this.rb2f8.Text = "Lunch";
+            this.rb2f8.UseVisualStyleBackColor = false;
+            // 
+            // rb3f8
+            // 
+            this.rb3f8.AutoSize = true;
+            this.rb3f8.BackColor = System.Drawing.Color.White;
+            this.rb3f8.Location = new System.Drawing.Point(955, 242);
+            this.rb3f8.Name = "rb3f8";
+            this.rb3f8.Size = new System.Drawing.Size(82, 27);
+            this.rb3f8.TabIndex = 11;
+            this.rb3f8.TabStop = true;
+            this.rb3f8.Text = "Dinner";
+            this.rb3f8.UseVisualStyleBackColor = false;
+            // 
+            // dgvMenu
+            // 
+            this.dgvMenu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMenu.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column4,
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgvMenu.Location = new System.Drawing.Point(520, 502);
+            this.dgvMenu.Name = "dgvMenu";
+            this.dgvMenu.RowHeadersWidth = 51;
+            this.dgvMenu.Size = new System.Drawing.Size(650, 150);
+            this.dgvMenu.TabIndex = 12;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Menu ID";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 150;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Day Name";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Meal Type";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Food Item";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // btn2f8
+            // 
+            this.btn2f8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2f8.Location = new System.Drawing.Point(826, 377);
+            this.btn2f8.Margin = new System.Windows.Forms.Padding(4);
+            this.btn2f8.Name = "btn2f8";
+            this.btn2f8.Size = new System.Drawing.Size(88, 47);
+            this.btn2f8.TabIndex = 13;
+            this.btn2f8.Text = "Save";
+            this.btn2f8.UseVisualStyleBackColor = true;
+            // 
+            // btn3f8
+            // 
+            this.btn3f8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3f8.Location = new System.Drawing.Point(949, 379);
+            this.btn3f8.Margin = new System.Windows.Forms.Padding(4);
+            this.btn3f8.Name = "btn3f8";
+            this.btn3f8.Size = new System.Drawing.Size(88, 47);
+            this.btn3f8.TabIndex = 14;
+            this.btn3f8.Text = "Delete";
+            this.btn3f8.UseVisualStyleBackColor = true;
+            this.btn3f8.Click += new System.EventHandler(this.btn3f8_Click_1);
+            // 
+            // MessMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(1540, 845);
+            this.Controls.Add(this.btn3f8);
+            this.Controls.Add(this.btn2f8);
+            this.Controls.Add(this.dgvMenu);
+            this.Controls.Add(this.rb3f8);
+            this.Controls.Add(this.rb2f8);
+            this.Controls.Add(this.btn1f8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbx1f8);
+            this.Controls.Add(this.rb1f8);
+            this.Controls.Add(this.cbx1f8);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MessMenu";
+            this.Text = "Form9";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl1f4;
+        private System.Windows.Forms.ComboBox cbx1f8;
+        private System.Windows.Forms.RadioButton rb1f8;
+        private System.Windows.Forms.TextBox tbx1f8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn1f8;
+        private System.Windows.Forms.RadioButton rb2f8;
+        private System.Windows.Forms.RadioButton rb3f8;
+        private System.Windows.Forms.DataGridView dgvMenu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button btn2f8;
+        private System.Windows.Forms.Button btn3f8;
+    }
+}
---
+++ UI/MessMenu.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using hostel_management_system_oop.BL;
+using hostel_management_system_oop.Model;
+using hostel_management_system_oop.UI; 
+namespace hostel_management_system_oop
+{
+    public partial class MessMenu : BaseForm 
+    {
+        private MessMenuBL menuBL = new MessMenuBL();
+        private int selectedMenuID = -1;
+
+        public MessMenu()
+        {
+            InitializeComponent();
+            SetupDataGridView();
+            LoadData(); 
+            LoadDayNames();
+        }
+
+       
+        public override void LoadData()
+        {
+            try
+            {
+                DataTable dt = menuBL.GetAllMenu();
+                dgvMenu.Rows.Clear();
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    dgvMenu.Rows.Add(
+                        row["MenuID"].ToString(),
+                        row["DayName"].ToString(),
+                        row["MealType"].ToString(),
+                        row["FoodItem"].ToString()
+                    );
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading menu: " + ex.Message);
+            }
+        }
+
+        // ← ADD THIS OVERRIDE METHOD (Replaces ClearForm)
+        public override void ClearForm()
+        {
+            cbx1f8.SelectedIndex = -1;
+            rb1f8.Checked = false;
+            rb2f8.Checked = false;
+            rb3f8.Checked = false;
+            tbx1f8.Clear();
+            tbx1f8.Focus();
+            selectedMenuID = -1;
+        }
+
+       
+        public override bool ValidateForm()
+        {
+            if (string.IsNullOrEmpty(cbx1f8.Text))
+            {
+                MessageBox.Show("Please select Day Name!");
+                cbx1f8.Focus();
+                return false;
+            }
+
+            string mealType = "";
+            if (rb1f8.Checked) mealType = "Breakfast";
+            else if (rb2f8.Checked) mealType = "Lunch";
+            else if (rb3f8.Checked) mealType = "Dinner";
+
+            if (string.IsNullOrEmpty(mealType))
+            {
+                MessageBox.Show("Please select Meal Type!");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(tbx1f8.Text))
+            {
+                MessageBox.Show("Please enter Food Item!");
+                tbx1f8.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void SetupDataGridView()
+        {
+            dgvMenu.AutoGenerateColumns = false;
+            dgvMenu.Columns.Clear();
+
+            // Add columns
+            dgvMenu.Columns.Add("MenuID", "ID");
+            dgvMenu.Columns.Add("DayName", "Day Name");
+            dgvMenu.Columns.Add("MealType", "Meal Type");
+            dgvMenu.Columns.Add("FoodItem", "Food Item");
+
+           
+            dgvMenu.Columns["MenuID"].Width = 60;
+            dgvMenu.Columns["DayName"].Width = 120;
+            dgvMenu.Columns["MealType"].Width = 100;
+            dgvMenu.Columns["FoodItem"].Width = 250;
+
+           
+            dgvMenu.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvMenu.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dgvMenu.AllowUserToAddRows = false;
+            dgvMenu.ReadOnly = true;
+            dgvMenu.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvMenu.RowHeadersVisible = false;
+
+            dgvMenu.CellClick += dgvMenu_CellClick;
+        }
+
+        private void LoadDayNames()
+        {
+            cbx1f8.Items.Clear();
+            cbx1f8.Items.Add("Monday");
+            cbx1f8.Items.Add("Tuesday");
+            cbx1f8.Items.Add("Wednesday");
+            cbx1f8.Items.Add("Thursday");
+            cbx1f8.Items.Add("Friday");
+            cbx1f8.Items.Add("Saturday");
+            cbx1f8.Items.Add("Sunday");
+            cbx1f8.SelectedIndex = 0;
+        }
+
+        private void dgvMenu_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvMenu.Rows[e.RowIndex];
+
+                selectedMenuID = Convert.ToInt32(row.Cells["MenuID"].Value);
+
+                cbx1f8.Text = row.Cells["DayName"].Value.ToString();
+
+                string mealType = row.Cells["MealType"].Value.ToString();
+                rb1f8.Checked = (mealType == "Breakfast");
+                rb2f8.Checked = (mealType == "Lunch");
+                rb3f8.Checked = (mealType == "Dinner");
+
+                tbx1f8.Text = row.Cells["FoodItem"].Value.ToString();
+            }
+        }
+
+       
+        private void btn2f8_Click(object sender, EventArgs e)
+        {
+           
+            if (!ValidateForm())
+                return;
+
+            string mealType = "";
+            if (rb1f8.Checked) mealType = "Breakfast";
+            else if (rb2f8.Checked) mealType = "Lunch";
+            else if (rb3f8.Checked) mealType = "Dinner";
+
+            MessMenuModel menu = new MessMenuModel();
+            menu.DayName = cbx1f8.Text;
+            menu.MealType = mealType;
+            menu.FoodItem = tbx1f8.Text;
+
+            string result = menuBL.SaveMenu(menu);
+
+            if (result == "SUCCESS")
+            {
+                MessageBox.Show("Menu Saved Successfully!");
+                ClearForm(); 
+                LoadData();  
+            }
+            else
+            {
+                MessageBox.Show("Error: " + result);
+            }
+        }
+
+        
+        private void btn1f8_Click(object sender, EventArgs e)
+        {
+            if (selectedMenuID == -1)
+            {
+                MessageBox.Show("Please click on a row to select a menu item first!");
+                return;
+            }
+
+           
+            if (!ValidateForm())
+                return;
+
+            string mealType = "";
+            if (rb1f8.Checked) mealType = "Breakfast";
+            else if (rb2f8.Checked) mealType = "Lunch";
+            else if (rb3f8.Checked) mealType = "Dinner";
+
+            MessMenuModel menu = new MessMenuModel();
+            menu.MenuID = selectedMenuID;
+            menu.DayName = cbx1f8.Text;
+            menu.MealType = mealType;
+            menu.FoodItem = tbx1f8.Text;
+
+            string result = menuBL.UpdateMenu(menu);
+
+            if (result == "SUCCESS")
+            {
+                MessageBox.Show("Menu Updated Successfully!");
+                ClearForm();
+                LoadData();
+                selectedMenuID = -1;
+            }
+            else
+            {
+                MessageBox.Show("Error: " + result);
+            }
+        }
+
+        // DELETE Button
+        private void btn3f8_Click_1(object sender, EventArgs e)
+        {
+            if (selectedMenuID == -1)
+            {
+                MessageBox.Show("Please click on a row to select a menu item first!");
+                return;
+            }
+
+            DialogResult confirm = MessageBox.Show("Are you sure you want to delete this menu item?", "Confirm Delete",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (confirm == DialogResult.Yes)
+            {
+                string result = menuBL.DeleteMenu(selectedMenuID);
+
+                if (result == "SUCCESS")
+                {
+                    MessageBox.Show("Menu Deleted Successfully!");
+                    ClearForm();
+                    LoadData();
+                }
+                else
+                {
+                    MessageBox.Show("Error: " + result);
+                }
+            }
+        }
+    }
+}
