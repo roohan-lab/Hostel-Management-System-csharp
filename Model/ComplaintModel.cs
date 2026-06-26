fb8fcf7dca7b92bd0899614f789855cd0f008377
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:23:38 +0500

    Add files via upload

---
+++ Model/ComplaintModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace hostel_management_system_oop.Model
+{
+    public class ComplaintModel
+    {
+        public int ComplaintID { get; set; }
+        public string RollNumber { get; set; }
+        public string ComplaintTitle { get; set; }
+        public string ComplaintDescription { get; set; }
+        public DateTime SubmissionDate { get; set; }
+        public string ComplaintStatus { get; set; }  
+        public int IsNewNotification { get; set; }  
+
+        public ComplaintModel()
+        {
+            SubmissionDate = DateTime.Now;
+            ComplaintStatus = "Pending";
+            IsNewNotification = 1;
+        }
+    }
+}
