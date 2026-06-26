16710ec048e55da5ea03d4671001bc39c1a8c611
roohan-lab <rohaanh616@gmail.com>
2026-06-26 17:26:36 +0500

    Add files via upload

---
+++ Model/MessAttendanceModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hostel_management_system_oop.Model
+{
+    public class MessAttendanceModel
+    {
+        public int MessAttendanceID { get; set; }
+        public string RollNumber { get; set; }
+        public DateTime AttendanceDate { get; set; }
+        public string MealType { get; set; }
+        public string Status { get; set; }
+    }
+}
